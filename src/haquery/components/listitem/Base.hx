dcd8a4355fbfe7d7256010ad6994bf5e9cfe72bf
yar3333 <yar3333@cb0898a6-47d2-4b67-8f47-642054c6c319>
2011-08-28 20:42:14 +0000

    List elem and custom events fixed.

--- src/haquery/components/listitem/Base.hx
+++ src/haquery/components/listitem/Base.hx
@@ -12,11 +12,11 @@ class Base extends HaqComponent
 {
 	override function connectEventHandlers(event:HaqEvent) : Void
 	{
-		trace("listitem[" + fullID + "] connectEventHandlers event = " + event.name);
+		//trace("listitem[" + fullID + "] connectEventHandlers event = " + event.name);
         if (parent != null && parent.parent != null)
         {
             var handlerName = event.component.id + '_' + event.name;
-            trace("handlerName = " + handlerName);
+            //trace("handlerName = " + handlerName);
             if (Reflect.hasMethod(parent.parent, handlerName))
             {
                 event.bind(parent.parent, Reflect.field(parent.parent, handlerName));
