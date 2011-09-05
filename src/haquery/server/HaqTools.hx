ca6a3bfa7e5bf162742a9b54f214b1d6d43bf760
Yaroslav Sivakov <yar3333@gmail.com>
2011-09-05 09:28:56 +0000

    * callClientMethod arguments php to js converter bugfix

--- src/haquery/server/HaqTools.hx
+++ src/haquery/server/HaqTools.hx
@@ -7,25 +7,33 @@ class HaqTools
 {
 	static function serverVarToClientString(v:Dynamic) : String
 	{
-		if (untyped __physeq__(v, null)) return 'null';
-		if (untyped __physeq__(v, true)) return 'true';
-		if (untyped __physeq__(v, false)) return 'false';
-		if (Type.typeof(v) == ValueType.TInt) return Std.string(v);
-		
-		if (Type.typeof(v) == ValueType.TObject)
-		{
-			if (Type.getClassName(Type.getClass(v)) == 'String')
-			{
-				return 'StringTools.unescape("' + StringTools.escape(v) + '")';
-			}
-			if (Type.getClassName(Type.getClass(v)) == 'Date')
-			{
-				var date : Date = cast(v, Date);
-				return "new Date(" + date.getTime() + ")";
-			}
-		}
-		
-		throw "Can't convert this type from server to client (typeof = " + Type.typeof(v) + ").";
+		switch (Type.typeof(v))
+        {
+            case ValueType.TNull:
+                return 'null';
+            
+            case ValueType.TBool:
+                return  cast(v, Bool) ? 'true' : 'false';
+            
+            case ValueType.TInt, ValueType.TFloat:
+                return Std.string(v);
+            
+            case ValueType.TObject:
+                return 'StringTools.unescape("' + StringTools.escape(v) + '")';
+            
+            case ValueType.TClass(clas):
+                if (Type.getClassName(clas) == 'String')
+                {
+                    return 'StringTools.unescape("' + StringTools.escape(v) + '")';
+                }
+                if (Type.getClassName(clas) == 'Date')
+                {
+                    var date : Date = cast(v, Date);
+                    return "new Date(" + date.getTime() + ")";
+                }
+            default:
+        }
+        throw "Can't convert this type from server to client (typeof = " + Type.typeof(v) + ").";
 	}
 	
 	public static function getCallClientFunctionString(func:String, params:Array<Dynamic>) : String
