6d5b2eb183e0d7e3f13ca4b8ccfb017116ea15a7
Yaroslav Sivakov <yar3333@gmail.com>
2011-09-19 08:58:05 +0000

    + hant.log

--- tools/haquery/src/Main.hx
+++ tools/haquery/src/Main.hx
@@ -21,8 +21,11 @@ class Main
             case 'post-build': 
                 haquery.postBuild();
                 
-            case 'class-paths':
-                haquery.getClassPaths();
+            case 'install':
+                haquery.install();
+            
+            case 'uninstall':
+                haquery.uninstall();
             
             default:
                 Lib.println('HaQuery building support and deploying tool.');
@@ -31,6 +34,8 @@ class Main
                 Lib.println('\t\tgen-orm <databaseConnectionString>    Generate tables-related classes to model folder.');
                 Lib.println('\t\tpre-build                             Do pre-build step.');
                 Lib.println('\t\tpost-build                            Do post-build step.');
+                Lib.println('\t\tinstall                               Replace official haxe.exe and librarires to HaxeMod.');
+                Lib.println('\t\tuninstall                             Restore official haxe.exe and libraries.');
                 return 1;
         }
         
