38f7148ea60e9de9ed4751284e144caad1e7d48e
yar3333 <yar3333@cb0898a6-47d2-4b67-8f47-642054c6c319>
2011-09-30 03:00:21 +0000

    + haquery.net

---
+++ tools/haquery.net/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace haquery_net
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var haquery = new haquery.Tasks();
+            
+            switch (args.Length > 0 ? args[0] : "")
+            {
+                case "gen-orm": 
+                    haquery.genOrm(args[1]);
+                    break;
+                
+                case "pre-build": 
+                    haquery.preBuild();
+                    break;
+                
+                case "post-build": 
+                    haquery.postBuild();
+                    break;
+                    
+                case "install":
+                    haquery.install();
+                    break;
+                
+                case "uninstall":
+                    haquery.uninstall();
+                    break;
+                
+                default:
+                    
+                    Console.WriteLine("HaQuery building support and deploying tool.");
+                    Console.WriteLine("Usage: haquery <command>");
+                    Console.WriteLine("\t where <command> may be:");
+                    Console.WriteLine("\t\tgen-orm <databaseConnectionString>    Generate tables-related classes to model folder.");
+                    Console.WriteLine("\t\tpre-build                             Do pre-build step.");
+                    Console.WriteLine("\t\tpost-build                            Do post-build step.");
+                    Console.WriteLine("\t\tinstall                               Replace official haxe.exe and librarires to HaxeMod.");
+                    Console.WriteLine("\t\tuninstall                             Restore official haxe.exe and libraries.");
+                    return 1;
+            }
+            
+            return 0;
+        }
+    }
+}
---
+++ tools/haquery.net/hant/Tasks.cs
@@ -0,0 +1,207 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+//using hant.Log;
+
+namespace haquery_net.hant
+{
+    class Tasks 
+    {
+        public delegate bool IncludeDelegate(string path);
+        
+        Log log;
+        
+        public Tasks(Log log)
+        {
+            this.log = log;
+        }
+        
+        public List<String> findFiles(string path, IncludeDelegate include)
+        {
+            var r = new List<String>();
+            
+            foreach (var file in Directory.GetDirectories(path))
+            {
+                if (include(path + '\\' + file))
+                {
+                    r.AddRange(findFiles(path + '\\' + file, include));
+                }
+            }
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                if (include(path + '\\' + file))
+                {
+                    r.Add(path + '\\' + file);
+                }
+            }
+            
+            return r;
+        }
+        
+        public void createDirectory(string path)
+        {
+            log.start("Create directory '" + path + "'");
+            try
+            {
+                var dirs = new List<string>(path.Split('\\'));
+                for (var i=0; i<dirs.Count; i++)
+                {
+                    var dir = String.Join("\\", dirs.GetRange(0, i + 1).ToArray());
+                    if (!dir.EndsWith(":"))
+                    {
+                        if (!Directory.Exists(dir))
+                        {
+                            Directory.CreateDirectory(dir);
+                        }
+                    }
+                }
+                log.finishOk();
+            }
+            catch (Exception e)
+            {
+                log.finishFail(e.Message);
+            }
+        }
+        
+        public void copyFolderContent(string fromFolder, string toFolder, IncludeDelegate include)
+        {
+            log.start("Copy directory '" + fromFolder + "' => '" + toFolder + "'");
+            try
+            {
+                foreach (var file in Directory.GetDirectories(fromFolder))
+                {
+                    if (include(fromFolder + '\\' + file))
+                    {
+                        copyFolderContent(fromFolder + '\\' + file, toFolder + '\\' + file, include);
+                    }
+                }
+                
+                foreach (var file in Directory.GetFiles(fromFolder))
+                {
+                    if (include(fromFolder + '\\' + file))
+                    {
+                        if (!Directory.Exists(toFolder)) Directory.CreateDirectory(toFolder);
+
+                        try
+                        {
+                            copyFile(fromFolder + '\\' + file, toFolder + '\\' + file);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                }
+                log.finishOk();
+            }
+            catch (Exception e)
+            {
+                log.finishFail(e.Message);
+            }
+        }
+        
+        public void copyFile(string src, string dst)
+        {
+            log.start("Copy file '" + src + "' => '" + dst + "'");
+            try
+            {
+                File.Copy(src, dst, true);
+                log.finishOk();
+            }
+            catch (Exception e)
+            {
+                log.finishFail(e.Message);
+            }
+        }
+        
+        public void rename(string path, string newpath)
+        {
+            log.start("Rename '" + path + "' => '" + newpath + "'");
+            try
+            {
+                if (File.Exists(path))
+                {
+                    if (File.Exists(newpath))
+                    {
+                        File.Delete(newpath);
+                    }
+                    File.Move(path, newpath);
+                }
+                else
+                {
+                    throw new Exception("File '" + path + "' not found.");
+                }
+                log.finishOk();
+            }
+            catch (Exception e)
+            {
+                log.finishFail(e.Message);
+            }
+        }
+        
+        public void deleteDirectory(string path)
+        {
+            log.start("Delete directory '" + path + "'");
+            try
+            {
+                foreach (var file in Directory.GetDirectories(path))
+                {
+                    deleteDirectory(path + '\\' + file);
+                }
+
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    deleteFile(path + '\\' + file);
+                }
+                
+                Directory.Delete(path);
+                log.finishOk();
+            }
+            catch (Exception e)
+            {
+                log.finishFail(e.Message);
+            }
+        }
+        
+        public void deleteFile(string path)
+        {
+            log.start("Delete file '" + path + "'");
+            try
+            {
+                File.Delete(path);
+                log.finishOk();
+            }
+            catch (Exception e)
+            {
+                log.finishFail(e.Message);
+            }
+        }
+        
+        /*public string getWindowsRegistryValue(string:key)
+        {
+            var dir = neko.System.Environment.GetEnvironmentVariable("TMP");
+		    if (dir == null)
+            {
+			    dir = "."; 		
+            }
+            var temp = dir + "/hant-tasks-get_windows_registry_value.txt";
+		    if (neko.Sys.command('regedit /E "' + temp + '" "' + key + '"') != 0) 
+            {
+			    // might fail without appropriate rights
+			    return null;
+		    }
+		    // it's possible that if registry access was disabled the proxy file is not created
+		    var content = try neko.io.File.getContent(temp) catch ( e : Dynamic ) return null;
+            content = content.Replace("\x00", "").Replace('\r', '').Replace('\\\\', '\\');
+		    neko.FileSystem.deleteFile(temp);
+            
+            var re = new EReg('^@="(.*)"$', 'm');
+            if (re.match(content)) return re.matched(1);
+            
+            return content;
+        }*/
+    }
+}
