b3ed9a53761381e886dc3b0a8783849275b4f29d
Yaroslav Sivakov <yar3333@gmail.com>
2011-09-30 03:00:21 +0000

    + haquery.net

---
+++ tools/haquery.net/haquery/Tasks.cs
@@ -0,0 +1,288 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace haquery_net.haquery
+{
+    class Tasks 
+    {
+        hant.Log log;
+        hant.Tasks hant;
+        
+        public Tasks()
+        {
+            log = new hant.Log(2);
+            hant = new hant.Tasks(log);
+        }
+        
+        void genImports()
+        {
+            log.start("Generate imports to 'src\\Imports.hx'");
+            
+            
+            var fo = new StreamWriter(new System.IO.FileStream("src\\Imports.hx", System.IO.FileMode.Create));
+            
+            foreach (var path in getClassPaths())
+            {
+                fo.Write("// " + path + "\n");
+                genImportsInner(fo, path);
+                fo.Write("\n");
+            }
+            
+            fo.Close();
+            
+            log.finishOk();
+        }
+        
+        void genImportsInner(System.IO.TextWriter fo, string srcPath)
+        {
+            var serverImports = hant.findFiles(srcPath, isServerFile);
+            var clientImports = hant.findFiles(srcPath, isClientFile);
+            
+            fo.Write("#if php\n");
+            foreach (var file in serverImports) fo.WriteLine(file2import(srcPath, file));
+            fo.Write("\n#else\n");
+            foreach (var file in clientImports) fo.WriteLine(file2import(srcPath, file));
+            fo.Write("\n#end\n");
+        }
+        
+        bool isServerFile(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return !path.EndsWith(".svn");
+            }
+            return path.EndsWith("\\Server.hx") || path.EndsWith("\\Bootstrap.hx");
+        }
+        
+        bool isClientFile(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return !path.EndsWith(".svn");
+            }
+            return path.EndsWith("\\Client.hx");
+        }
+        
+        bool isSupportFile(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return !path.EndsWith(".svn");
+            }
+            return !path.EndsWith(".hx") && !path.EndsWith(".hxproj");
+        }
+        
+        string file2import(string basePath, string file)
+        {
+            if (file.StartsWith(basePath))
+            {
+                file = file.Substring(basePath.Length + 1);
+            }
+            
+            return "import " + Path.GetFileNameWithoutExtension(file).Replace('\\', '.') + ';';
+        }
+        
+        bool isNotSvn(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return !path.EndsWith(".svn");
+            }
+            return true;
+        }
+        
+        public List<string> getClassPaths()
+        {
+            var r = new List<String>();
+            var files = Directory.GetFiles("");
+            foreach (var file in files)
+            {
+                if (file.EndsWith(".hxproj"))
+                {
+                    var xml = new System.Xml.XmlDocument();
+                    xml.Load(file);
+                    foreach (System.Xml.XmlElement elem in xml.SelectNodes("/project/classpaths/class"))
+                    {
+                        if (elem.GetAttribute("path") != null)
+                        {
+                            r.Add(elem.GetAttribute("path"));
+                        }
+                    }
+                }
+            }
+            return r;
+        }
+        
+        // -------------------------------------------------------------------------------
+        
+        void buildJs()
+        {
+            log.start("Build client to 'bin\\haquery\\client\\haquery.js'");
+            
+            hant.createDirectory("bin\\haquery\\client");
+            
+            var pars = new List<String>();
+            foreach (var path in getClassPaths())
+            {
+                pars.Add("-cp"); pars.Add(path);
+            }
+            pars.Add("-js");
+            pars.Add("bin\\haquery\\client\\haquery.js");
+            pars.Add("-main"); pars.Add("Main");
+            pars.Add("-debug");
+
+            System.Diagnostics.Process.Start("haxe", String.Join(" ", pars.ToArray()));
+            
+            log.finishOk();
+        }
+
+        static string getExeDir()
+        {
+            System.Reflection.Assembly a = System.Reflection.Assembly.GetEntryAssembly();
+            return System.IO.Path.GetDirectoryName(a.Location);
+        }
+        
+        public void genOrm(string databaseConnectionString)
+        {
+            log.start("Generate ORM classes to 'models'");
+            
+            
+            System.Diagnostics.Process.Start(
+                "php", String.Join(" ", new string[] { getExeDir() + "\\orm\\index.php", databaseConnectionString, "src" })
+            );
+            
+            log.finishOk();
+        }
+        
+        public void preBuild()
+        {
+            log.start("Do pre-build step");
+            
+            genImports();
+            
+            log.finishOk();
+        }
+        
+        public void postBuild()
+        {
+            log.start("Do post-build step");
+            
+            buildJs();
+            
+            foreach (var path in getClassPaths())
+            {
+                hant.copyFolderContent(path, "bin", isSupportFile);
+            }
+            
+            log.finishOk();
+        }
+        
+        public string getHaxePath()
+        {
+            var r = System.Environment.GetEnvironmentVariable("HAXEPATH");
+            
+            if (r == null)
+            {
+                throw new Exception("HaXe not found (HAXEPATH environment variable not set).");
+            }
+
+            r = r.TrimEnd('\\') + '\\';
+            
+            if (!File.Exists(r + "haxe.exe"))
+            {
+                throw new Exception("HaXe not found (file '" + r + "haxe.exe' do not exist).");
+            }
+            
+            return r;
+        }
+        
+        public void install()
+        {
+            installFlashDevelopTemplates();
+            installHaxeMod();
+        }
+        
+        void installFlashDevelopTemplates()
+        {
+            log.start("Install FlashDevelop templates");
+            
+            var srcPath = getExeDir() + "\\flashdevelop";
+            var haxePath = getHaxePath();
+            var userLocalPath = System.Environment.GetEnvironmentVariable("LOCALAPPDATA") != null 
+                ? System.Environment.GetEnvironmentVariable("LOCALAPPDATA") 
+                : System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Local Settings\\Application Data";
+            var flashDevelopUserDataPath = userLocalPath + "\\FlashDevelop";
+            hant.copyFolderContent(srcPath, flashDevelopUserDataPath, isNotSvn);
+            
+            var projectFilePath = flashDevelopUserDataPath + "\\Projects\\380 HaXe - HaQuery Project\\Project.hxproj";
+            var projectFileContent = File.ReadAllText(projectFilePath);
+            File.WriteAllText(projectFilePath, projectFileContent.Replace("{HaQuerySrcPath}",  Path.GetFullPath(getExeDir() + "\\..\\src")));
+            
+            log.finishOk();
+        }
+        
+        void installHaxeMod()
+        {
+            log.start("Install HaxeMod");
+            
+            var haxePath = getHaxePath();
+            
+            /*if (!Directory.Exists(haxePath + 'haxe.exe.official'))
+            {
+                hant.rename(haxePath + 'haxe.exe', haxePath + 'haxe.exe.official');
+            }
+            File.Copy(getExeDir() + '\\haxemod\\haxe.exe', haxePath + 'haxe.exe');*/
+            
+            if (!Directory.Exists(haxePath + "std.official"))
+            {
+                hant.rename(haxePath + "std", haxePath + "std.official");
+                hant.copyFolderContent(getExeDir() + "\\haxemod\\std", haxePath + "std", new hant.Tasks.IncludeDelegate(getTrue));
+            }
+            
+            log.finishOk();
+        }
+
+        static bool getTrue(string path)
+        {
+            return true;
+        }
+        
+        public void uninstall()
+        {
+            //uninstallFlashDevelopTemplates();
+            uninstallHaxeMod();
+        }
+        
+        /*function uninstallFlashDevelopTemplates()
+        {
+            log.start('Uninstall FlashDevelop templates');
+            
+            log.finishOk();
+        }*/
+        
+        void uninstallHaxeMod()
+        {
+            log.start("Uninstall HaxeMod");
+            
+            var haxePath = getHaxePath();
+
+            /*if (!Directory.Exists(haxePath + 'haxe.exe.official'))
+            {
+                log.finishFail("HaxeMod does not installed.");
+            }
+            hant.rename(haxePath + 'haxe.exe.official', haxePath + 'haxe.exe');*/
+            
+            if (Directory.Exists(haxePath + "std.official"))
+            {
+                hant.deleteDirectory(haxePath + "std");
+                hant.rename(haxePath + "std.official", haxePath + "std");
+            }
+            
+            log.finishOk();
+        }
+        
+        
+    }
+}
