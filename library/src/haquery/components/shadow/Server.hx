27ded3d2dc3c7704a5137a56cbda67df376181e7
yar3333 <yar3333@cb0898a6-47d2-4b67-8f47-642054c6c319>
2011-10-07 08:11:28 +0000

    + shadow component

---
+++ library/src/haquery/components/shadow/Server.hx
@@ -0,0 +1,13 @@
+package components.shadow;
+
+import haquery.server.HaqComponent;
+
+class Server extends HaqComponent
+{
+    public var cssClass : String;
+	
+    function preRender()
+	{
+        if (cssClass!=null) q('#shadow').addClass(cssClass);
+	}
+}
