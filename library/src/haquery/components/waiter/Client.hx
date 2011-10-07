a68c9caee92a9ee218e7c00bcda2793a3e9c083e
Yaroslav Sivakov <yar3333@gmail.com>
2011-10-07 08:08:16 +0000

    + waiter component

---
+++ library/src/haquery/components/waiter/Client.hx
@@ -0,0 +1,31 @@
+package components.waiter;
+
+import haquery.client.HaqComponent;
+
+class Client extends HaqComponent
+{
+	public function show()
+	{
+        var shadow = q('#shadow');
+		var animation = q('#animation');
+		
+		var selector = q('#shadow').attr('selector');
+        var element = parent.q(selector);
+        
+        shadow.css('top', element.offset().top);
+        shadow.css('left', element.offset().left);
+        shadow.width(element.outerWidth());
+        shadow.height(element.outerHeight());
+        shadow.show();
+        
+        animation.css('top', element.offset().top + element.outerHeight() / 2 - animation.height() / 2);
+        animation.css('left', element.offset().left + element.outerWidth() / 2 - animation.width() / 2);
+        animation.show();
+	}
+	
+	public function hide() : Void
+	{
+		q('#shadow').hide();
+		q('#animation').hide();
+	}
+}
