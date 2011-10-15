ffb4b622c68053ec6616d207d3b47e4735eae9fa
yar3333 <yar3333@cb0898a6-47d2-4b67-8f47-642054c6c319>
2011-10-15 20:28:04 +0000

    * run * version ++

--- library/src/haquery/base/HaqDefines.hx
+++ library/src/haquery/base/HaqDefines.hx
@@ -2,7 +2,7 @@ package haquery.base;
 
 class HaqDefines 
 {
-	public static inline var VERSION = 2.3; 
+	public static inline var VERSION = 2.4; 
 	
 	public static var folders = {
 		 pages : 'pages'
