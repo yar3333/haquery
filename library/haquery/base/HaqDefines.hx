47aa18e73b9aaab4f4f34e35a7984b54b6d464cf
Yaroslav Sivakov <yar3333@gmail.com>
2011-10-22 23:56:08 +0000

    * VERSION 2.4

--- library/haquery/base/HaqDefines.hx
+++ library/haquery/base/HaqDefines.hx
@@ -2,7 +2,7 @@ package haquery.base;
 
 class HaqDefines 
 {
-	public static inline var VERSION = 2.4; 
+	public static inline var VERSION = 2.5;
 	
 	public static var folders = {
 		 pages : 'pages'
