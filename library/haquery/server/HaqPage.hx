a5a252a38c2309edf63801e40e2e413ec9f203ea
Yaroslav Sivakov <yar3333@gmail.com>
2011-10-28 16:12:53 +0400

    * page without <head> support

--- library/haquery/server/HaqPage.hx
+++ library/haquery/server/HaqPage.hx
@@ -29,57 +29,66 @@ class HaqPage extends HaqComponent
     
     public function insertStyles(links:Array<String>)
     {
-        var text = Lambda.map(links, function(path:String):String { return getStyleLink(path); } ).join('\n        ');
+        var text = Lambda.map(links, function(path) return getStyleLink(path)).join('\n        ');
         var heads = Lib.toHaxeArray(doc.find(">html>head"));
-        if (heads.length == 0)
+        if (heads.length > 0)
         {
-            throw "HaqPage.insertStyles(): head tag not found.";
-        }
-        var head : HaqXmlNodeElement = heads[0];
-        var child : HaqXmlNodeElement = null;
-        var children = Lib.toHaxeArray(head.children);
-        if (children.length > 0)
-        {
-            child = head.children[0];
-            while (child != null && child.name != 'link' && (child.getAttribute('rel') != 'stylesheet' || child.getAttribute('type') != 'text/css'))
+            var head : HaqXmlNodeElement = heads[0];
+            var child : HaqXmlNodeElement = null;
+            var children = Lib.toHaxeArray(head.children);
+            if (children.length > 0)
             {
-                child = child.getNextSiblingElement();
+                child = head.children[0];
+                while (child != null && child.name != 'link' && (child.getAttribute('rel') != 'stylesheet' || child.getAttribute('type') != 'text/css'))
+                {
+                    child = child.getNextSiblingElement();
+                }
             }
+            head.addChild(new HaqXmlNodeText(text + '\n        '), child);
         }
-        head.addChild(new HaqXmlNodeText(text + '\n        '), child);
+        else
+        {
+            doc.addChild(new HaqXmlNodeText(text + '\n'));
+        }
     }
     
     public function insertScripts(links:Array<String>)
     {
-        var text = Lambda.map(links, function(path:String):String { return getScriptLink(path); } ).join('\n        ');
+        var text = Lambda.map(links, function(path) return getScriptLink(path)).join('\n        ');
         var heads = Lib.toHaxeArray(doc.find(">html>head"));
-        if (heads.length == 0)
+        if (heads.length > 0)
         {
-            throw "HaqPage.insertScripts(): head tag not found.";
-        }
-        var head : HaqXmlNodeElement = heads[0];
-        var child : HaqXmlNodeElement = null;
-        var children = Lib.toHaxeArray(head.children);
-        if (children.length > 0)
-        {
-            child = head.children[0];
-            while (child != null && child.name != 'script')
+            var head : HaqXmlNodeElement = heads[0];
+            var child : HaqXmlNodeElement = null;
+            var children = Lib.toHaxeArray(head.children);
+            if (children.length > 0)
             {
-                child = child.getNextSiblingElement();
+                child = head.children[0];
+                while (child != null && child.name != 'script')
+                {
+                    child = child.getNextSiblingElement();
+                }
             }
+            head.addChild(new HaqXmlNodeText('    ' + text + '\n    '), child);
         }
-        head.addChild(new HaqXmlNodeText('    ' + text + '\n    '), child);
+        else
+        {
+            doc.addChild(new HaqXmlNodeText(text + '\n'));
+        }
     }
     
     public function insertInitInnerBlock(text:String)
     {
         var bodyes = Lib.toHaxeArray(doc.find(">html>body"));
-        if (bodyes.length == 0)
+        if (bodyes.length > 0)
         {
-            throw "HaqPage.insertInitInnerBlock(): body tag not found.";
+            var body : HaqXmlNodeElement = bodyes[0];
+            body.addChild(new HaqXmlNodeText("\n        " + text.replace('\n', '\n        ') + '\n    '));
         }
-        var body : HaqXmlNodeElement = bodyes[0];
-        body.addChild(new HaqXmlNodeText("\n        " + text.replace('\n', '\n        ') + '\n    '));
+        else
+        {
+            doc.addChild(new HaqXmlNodeText("\n" + text + '\n'));
+        }
     }
     
     static function getScriptLink(path:String) : String
