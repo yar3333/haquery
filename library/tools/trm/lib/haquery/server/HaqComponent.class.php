bb58ff51c240cb48d08b90ec0d3a9af162f3ee3a
Yaroslav <Yaroslav@YHOST.beeline>
2012-01-15 03:20:59 +0400

    * unneccessary Template class generation fixed (when Server and Client component classes not exists)

--- library/tools/trm/lib/haquery/server/HaqComponent.class.php
+++ library/tools/trm/lib/haquery/server/HaqComponent.class.php
@@ -20,40 +20,8 @@ class haquery_server_HaqComponent extends haquery_base_HaqComponent {
 		$this->manager = $manager;
 		$this->doc = $doc;
 		$this->parentNode = $parentNode;
-		$this->loadParamsToObjectFields($params, $this->getFieldsToLoadParams());
-		$this->createEvents();
-		$this->createChildComponents();
-		if(Reflect::isFunction(Reflect::field($this, "init"))) {
-			Reflect::callMethod($this, Reflect::field($this, "init"), new _hx_array(array()));
-		}
-		$GLOBALS['%s']->pop();
-	}
-	public function getFieldsToLoadParams() {
-		$GLOBALS['%s']->push("haquery.server.HaqComponent::getFieldsToLoadParams");
-		$製pos = $GLOBALS['%s']->length;
-		$restrictedFields = Reflect::fields(Type::createEmptyInstance(Type::resolveClass("haquery.server.HaqComponent")));
-		$r = new Hash();
-		{
-			$_g = 0; $_g1 = Reflect::fields($this);
-			while($_g < $_g1->length) {
-				$field = $_g1[$_g];
-				++$_g;
-				if(!Reflect::isFunction(Reflect::field($this, $field)) && !Lambda::has($restrictedFields, $field, null) && !StringTools::startsWith($field, "event_")) {
-					$r->set(strtolower($field), $field);
-				}
-				unset($field);
-			}
-		}
-		{
-			$GLOBALS['%s']->pop();
-			return $r;
-		}
-		$GLOBALS['%s']->pop();
-	}
-	public function loadParamsToObjectFields($params, $fields) {
-		$GLOBALS['%s']->push("haquery.server.HaqComponent::loadParamsToObjectFields");
-		$製pos = $GLOBALS['%s']->length;
 		if($params !== null) {
+			$fields = $manager->getFieldsToLoadParams($this);
 			if(null == $params) throw new HException('null iterable');
 			$蜴t = $params->keys();
 			while($蜴t->hasNext()) {
@@ -85,78 +53,18 @@ class haquery_server_HaqComponent extends haquery_base_HaqComponent {
 				unset($v);
 			}
 		}
+		$this->createEvents();
+		$this->createChildComponents();
+		if(Reflect::isFunction(Reflect::field($this, "init"))) {
+			Reflect::callMethod($this, Reflect::field($this, "init"), new _hx_array(array()));
+		}
 		$GLOBALS['%s']->pop();
 	}
 	public function createChildComponents() {
 		$GLOBALS['%s']->push("haquery.server.HaqComponent::createChildComponents");
 		$製pos = $GLOBALS['%s']->length;
 		if($this->doc !== null) {
-			$this->createChildComponents_inner($this->doc);
-		}
-		$GLOBALS['%s']->pop();
-	}
-	public function createChildComponents_inner($baseNode) {
-		$GLOBALS['%s']->push("haquery.server.HaqComponent::createChildComponents_inner");
-		$製pos = $GLOBALS['%s']->length;
-		$i = 0;
-		while($i < count($baseNode->children)) {
-			$node = $baseNode->children[$i];
-			haquery_server_Lib::assert($node->name !== "haq:placeholder", null, _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 103, "className" => "haquery.server.HaqComponent", "methodName" => "createChildComponents_inner")));
-			haquery_server_Lib::assert($node->name !== "haq:content", null, _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 104, "className" => "haquery.server.HaqComponent", "methodName" => "createChildComponents_inner")));
-			$this->createChildComponents_inner($node);
-			if(StringTools::startsWith($node->name, "haq:")) {
-				$node->component = $this->manager->createComponent($this, $node->name, $node->getAttribute("id"), php_Lib::hashOfAssociativeArray($node->getAttributesAssoc()), $node);
-			}
-			$i++;
-			unset($node);
-		}
-		$GLOBALS['%s']->pop();
-	}
-	public function prepareDocToRender($baseNode) {
-		$GLOBALS['%s']->push("haquery.server.HaqComponent::prepareDocToRender");
-		$製pos = $GLOBALS['%s']->length;
-		$i = 0;
-		while($i < count($baseNode->children)) {
-			$node = $baseNode->children[$i];
-			if(StringTools::startsWith($node->name, "haq:")) {
-				if($node->component === null) {
-					haxe_Log::trace("Component is null: " . $node->name, _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 126, "className" => "haquery.server.HaqComponent", "methodName" => "prepareDocToRender")));
-					haquery_server_Lib::assert(false, null, _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 127, "className" => "haquery.server.HaqComponent", "methodName" => "prepareDocToRender")));
-				}
-				if($node->component->visible) {
-					$this->prepareDocToRender($node);
-					$text = trim($node->component->render(), null);
-					$prev = $node->getPrevSiblingNode();
-					if($prev instanceof HaqXmlNodeText) {
-						$re = new haquery_EReg("(?:^|\x0A)([ ]+)\$", "s");
-						if($re->match(_hx_deref(($prev))->text)) {
-							$text = str_replace("\x0A", "\x0A" . $re->matched(1), $text);
-						}
-						unset($re);
-					}
-					$node->parent->replaceChild($node, new HaqXmlNodeText($text));
-					unset($text,$prev);
-				} else {
-					$node->remove();
-					$i--;
-				}
-			} else {
-				$this->prepareDocToRender($node);
-				$nodeID = $node->getAttribute("id");
-				if($nodeID !== null && $nodeID !== "") {
-					$node->setAttribute("id", $this->prefixID . $nodeID);
-				}
-				if($node->name === "label") {
-					$nodeFor = $node->getAttribute("for");
-					if($nodeFor !== null && $nodeFor !== "") {
-						$node->setAttribute("for", $this->prefixID . $nodeFor);
-					}
-					unset($nodeFor);
-				}
-				unset($nodeID);
-			}
-			$i++;
-			unset($node);
+			$this->manager->createChildComponents($this, $this->doc);
 		}
 		$GLOBALS['%s']->pop();
 	}
@@ -164,9 +72,9 @@ class haquery_server_HaqComponent extends haquery_base_HaqComponent {
 		$GLOBALS['%s']->push("haquery.server.HaqComponent::render");
 		$製pos = $GLOBALS['%s']->length;
 		if(haquery_server_Lib::$config->isTraceComponent) {
-			haxe_Log::trace("render " . $this->fullID, _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 172, "className" => "haquery.server.HaqComponent", "methodName" => "render")));
+			haxe_Log::trace("render " . $this->fullID, _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 81, "className" => "haquery.server.HaqComponent", "methodName" => "render")));
 		}
-		$this->prepareDocToRender($this->doc);
+		$this->manager->prepareDocToRender($this->prefixID, $this->doc);
 		$r = trim($this->doc->toString(), "\x0D\x0A");
 		{
 			$GLOBALS['%s']->pop();
@@ -187,7 +95,7 @@ class haquery_server_HaqComponent extends haquery_base_HaqComponent {
 			return $query;
 		}
 		if($query instanceof HaqXmlNodeElement) {
-			haquery_server_Lib::assert(!haquery_server_Lib::$isPostback, "Calling of the HaqComponent.q() with HaqXmlNodeElement parameter do not possible on the postback.", _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 190, "className" => "haquery.server.HaqComponent", "methodName" => "q")));
+			haquery_server_Lib::assert(!haquery_server_Lib::$isPostback, "Calling of the HaqComponent.q() with HaqXmlNodeElement parameter do not possible on the postback.", _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 99, "className" => "haquery.server.HaqComponent", "methodName" => "q")));
 			{
 				$裨mp = new haquery_server_HaqQuery($this->prefixID, "", php_Lib::toPhpArray(new _hx_array(array($query))));
 				$GLOBALS['%s']->pop();
@@ -208,7 +116,7 @@ class haquery_server_HaqComponent extends haquery_base_HaqComponent {
 	public function callClientMethod($method, $params) {
 		$GLOBALS['%s']->push("haquery.server.HaqComponent::callClientMethod");
 		$製pos = $GLOBALS['%s']->length;
-		haquery_server_Lib::assert(haquery_server_Lib::$isPostback, "HaqComponent.callClientMethod() allowed on the postback only.", _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 205, "className" => "haquery.server.HaqComponent", "methodName" => "callClientMethod")));
+		haquery_server_Lib::assert(haquery_server_Lib::$isPostback, "HaqComponent.callClientMethod() allowed on the postback only.", _hx_anonymous(array("fileName" => "HaqComponent.hx", "lineNumber" => 114, "className" => "haquery.server.HaqComponent", "methodName" => "callClientMethod")));
 		$funcName = haquery_server_HaqComponent_0($this, $method, $params);
 		haquery_server_HaqInternals::addAjaxResponse(haquery_base_HaqTools::getCallClientFunctionString($funcName, $params) . ";");
 		$GLOBALS['%s']->pop();
@@ -228,6 +136,18 @@ class haquery_server_HaqComponent extends haquery_base_HaqComponent {
 			$GLOBALS['%s']->pop();
 			return $裨mp;
 		}
+		$GLOBALS['%s']->pop();
+	}
+	public function registerScript($url) {
+		$GLOBALS['%s']->push("haquery.server.HaqComponent::registerScript");
+		$製pos = $GLOBALS['%s']->length;
+		$this->manager->registerScript($this->tag, $url);
+		$GLOBALS['%s']->pop();
+	}
+	public function registerStyle($url) {
+		$GLOBALS['%s']->push("haquery.server.HaqComponent::registerStyle");
+		$製pos = $GLOBALS['%s']->length;
+		$this->manager->registerStyle($this->tag, $url);
 		$GLOBALS['%s']->pop();
 	}
 	public function __call($m, $a) {
