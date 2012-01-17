c6486adb348541466ea5995aecc7d4616406dc10
Yaroslav <Yaroslav@YHOST.beeline>
2012-01-17 15:52:58 +0400

    * splitter refactoring

--- library/haquery/components/splitter/Client.hx
+++ library/haquery/components/splitter/Client.hx
@@ -1,56 +1,71 @@
+#if js
+
 package haquery.components.splitter;
 
 import haquery.client.HaqQuery;
-import haquery.client.HaqEvent;
+import haquery.client.Lib;
 import haquery.client.HaqComponent;
+import haquery.client.HaqEvent;
 
 class Client extends HaqComponent
 {
+    static inline var HORIZONTAL = "horizontal";
+    static inline var VERTICAL = "vertical";
+	
+	/**
+     * May be 'horizontal' (by default) or 'vertical'.
+     */
+	public var type : String;
+	
+	public var k : Float;
+	public var size : Int;
+
     var event_change : HaqEvent;
+	
+	var a : HaqQuery;
+	var b : HaqQuery;
 
-    var k : Float;
-    var size : Int;
-
-    var dragMode : Bool;
+    var mouseupHandler : Dynamic->Void;
+    var mousemoveHandler : Dynamic->Void;
+	
+	var dragMode : Bool;
     var dragMouseX : Int;
     var dragMouseY : Int;
     var dragK : Float;
-    
-    var mouseupHandler : Dynamic -> Void;
-    var mousemoveHandler : Dynamic -> Void;
-    
-    public function new ()
+	
+	function new()
+	{
+		super();
+		type = HORIZONTAL;
+		
+		k = 0.5;
+		size = 1;
+	}
+	
+	function init()
     {
-        super();
-
-        k = 0.5;
-        size = 1;
-        
-        dragMode = false;
-        dragMouseX = 0;
-        dragMouseY = 0;
-        dragK = 0.0;
-    }
-    
-    
-    function init()
-    {
-        untyped q('#s,#f').disableSelection();
+        if (type != HORIZONTAL && type != VERTICAL)
+		{
+			trace("components.splitter - unknow type = " + type);
+		}
+		
+		
+		a = new HaqQuery(q('#a').val()); q('#a').remove();
+        b = new HaqQuery(q('#b').val()); q('#b').remove();
+		
+		untyped q('#s,#f').disableSelection();
         setK(k);
     }
 
-    private function setK(k:Float) : Void
+    function setK(k)
     {
         this.k = k;
-
-        var a = parent.q('#'+q('#a').html());
-        var b = parent.q('#'+q('#b').html());
 
         a.width(1);
         b.width(1);
 
         var fullSize = getFullSize();
-        var sizeA = Math.floor(fullSize*k) - 2;
+        var sizeA = Math.floor(fullSize * k) - 2;
         var sizeB = fullSize - sizeA - 2;
         a.width(sizeA);
         b.width(sizeB);
@@ -58,12 +73,9 @@ class Client extends HaqComponent
         event_change.call([sizeA, sizeB]);
     }
 
-    private function getFullSize()
+    function getFullSize()
     {
-        var s = this.q('#s');
-        var a = this.parent.q('#'+this.q('#a').html());
-        var b = this.parent.q('#'+this.q('#b').html());
-        
+        var s = q('#s');
         var r = s.parent().width() 
               - s.width()
               - Std.parseInt(s.css('margin-left').split('px')[0])
@@ -75,56 +87,57 @@ class Client extends HaqComponent
         return r;
     }
 
-    public function setSize(size:Int)
+    public function setSize(size)
     {
         this.size = size;
-        var splitter = this.q('#s');
+        var splitter = q('#s');
         if (splitter.hasClass('splitter-vertical'))
             splitter.css('height', size+'px');
         else if (splitter.hasClass('splitter-horizontal'))
             splitter.css('width', size+'px');
     }
 
-
-    private function s_mousedown(e)
+    function s_mousedown(e)
     {
-        this.dragMode = true;
-        this.dragMouseX = e.pageX;
-        this.dragMouseY = e.pageY;
-        this.dragK = this.k;
+        dragMode = true;
+        dragMouseX = e.pageX;
+        dragMouseY = e.pageY;
+        dragK = k;
 
-        var pos = this.q('#f').parent().offset();
-        this.q('#f')
+        var pos = q('#f').parent().offset();
+        q('#f')
             .css('left', pos.left+'px')
             .css('top', pos.top+'px')
-            .width(this.getFullSize())
-            .height(this.size)
+            .width(getFullSize())
+            .height(size)
             .show();
 
         var self = this;
 
         mouseupHandler = function(e) { self.mouseup(e); };
-        new HaqQuery(js.Lib.document).mouseup(this.mouseupHandler);
+        new HaqQuery(Lib.document).mouseup(mouseupHandler);
 
         mousemoveHandler = function(e) { self.mousemove(e); };
-        new HaqQuery(js.Lib.document).mousemove(this.mousemoveHandler);
+        new HaqQuery(Lib.document).mousemove(mousemoveHandler);
     }
     
-    private function mouseup(e)
+    function mouseup(e)
     {
-        if (!this.dragMode) return;
-        this.dragMode = false;
-        new HaqQuery(js.Lib.document).unbind('mouseup', mouseupHandler);
-        new HaqQuery(js.Lib.document).unbind('mousemove', mousemoveHandler);
-        this.q('#f').hide();
+        if (!dragMode) return;
+        dragMode = false;
+        new HaqQuery(Lib.document).unbind('mouseup', mouseupHandler);
+        new HaqQuery(Lib.document).unbind('mousemove', mousemoveHandler);
+        q('#f').hide();
     }
 
-    private function mousemove(e)
+    function mousemove(e)
     {
-        if (!this.dragMode) return;
-        var dx = e.pageX - this.dragMouseX;
-        var dy = e.pageY - this.dragMouseY;
-        var k = this.dragK + dx / this.getFullSize();
-        this.setK(k);
+        if (!dragMode) return;
+        var dx = e.pageX - dragMouseX;
+        var dy = e.pageY - dragMouseY;
+        var k = dragK + dx / getFullSize();
+        setK(k);
     }
 }
+
+#end
