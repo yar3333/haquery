7a1cbe38a82884ca21de2b1fee6247937409a06a
Yaroslav Sivakov <yar3333@gmail.com>
2012-02-17 18:09:12 +0400

    * send template data from server to client

--- library/haquery/client/HaqTemplate.hx
+++ library/haquery/client/HaqTemplate.hx
@@ -2,6 +2,16 @@ package haquery.client;
 
 class HaqTemplate extends haquery.base.HaqTemplate
 { 
-	public var elemID_serverHandlers: Hash<Array<String>>;
-	public var clas : Class<HaqComponent>;
+	public var clientClass(default, null) : Class<HaqComponent>;
+	
+	/**
+	 * elemID => handlers
+	 */
+	public var serverHandlers(default, null) : Hash<Array<String>>;
+	
+	public function new(imports:Array<String>, clientClassName:String)
+	{
+		super(imports);
+		this.clientClass = Type.resolveClass(clientClassName);
+	}
 }
