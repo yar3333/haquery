dca8f567f757e7353bc35f2b7b34b2f38775bdf4
Yaroslav Sivakov <yar3333@gmail.com>
2012-02-20 20:15:43 +0400

    * client.HaqQuery fixes

--- library/haquery/client/HaqComponent.hx
+++ library/haquery/client/HaqComponent.hx
@@ -3,7 +3,10 @@
 package haquery.client;
 
 import haquery.client.Lib;
-import haquery.client.HaqQuery;
+//import haquery.client.HaqQuery;
+import js.JQuery;
+
+using haquery.StringTools;
 
 class HaqComponent extends haquery.base.HaqComponent
 {
@@ -47,11 +50,43 @@ class HaqComponent extends haquery.base.HaqComponent
 	
 	public function q(selector:String, ?base:Dynamic) : HaqQuery
 	{
+		var prefixCssClass = fullTag.replace(".", "_") + HaqDefines.DELIMITER;
+		
 		if (selector != null && prefixID != '')
 		{
 			selector = StringTools.replace(selector, '#', '#' + prefixID);
 		}
-		return new HaqQuery(selector, base);
+		
+		var jq = new JQuery(selector, base);
+		
+		untyped __js__("
+jq.haqueryPrefixCssClass = prefixCssClass;
+
+jq.globalizeClassName = function(className)
+{
+	return className.replace(/~/, this.haqueryPrefixCssClass);
+};
+
+var oldAddClass = jq.addClass;
+jq.addClass = function(name)
+{
+	return oldAddClass.call(this, this.globalizeClassName(name));
+}
+
+var oldRemoveClass = jq.removeClass;
+jq.removeClass = function(name)
+{
+	return oldRemoveClass.call(this, this.globalizeClassName(name));
+}
+
+var oldHasClass = jq.hasClass;
+jq.hasClass = function(name)
+{
+	return oldHasClass.call(this, this.globalizeClassName(name));
+}
+		");
+		
+		return jq;
 	}
     
     private function connectElemEventHandlers() : Void
--- library/haquery/client/HaqQuery.hx
+++ library/haquery/client/HaqQuery.hx
@@ -1,34 +1,3 @@
 package haquery.client;
 
-import js.JQuery;
-
-class HaqQuery extends JQuery
-{
-    public var prefixCssClass : String;
-    
-    public function new(prefixCssClass:String, ?j:Dynamic)
-    {
-		this.prefixCssClass = prefixCssClass;
-		super(j);
-    }
-	
-	private function globalizeClassName(className:String) : String
-	{
-        return ~/\b~/.replace(className, prefixCssClass);
-	}
-	
-	override public function addClass(className:String) : JQuery 
-	{
-		return super.addClass(globalizeClassName(className));
-	}
-	
-	override public function removeClass(className:String) : JQuery 
-	{
-		return super.removeClass(globalizeClassName(className));
-	}
-	
-	override public function hasClass(className:String) : Bool 
-	{
-		return super.hasClass(globalizeClassName(className));
-	}
-}
+typedef HaqQuery = js.JQuery;
