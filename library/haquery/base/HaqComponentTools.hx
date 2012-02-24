b59e72b17609e520980172c7933f2c740ae54008
Yaroslav <Yaroslav@YHOST.beeline>
2012-02-24 20:38:31 +0400

    * template class creating on the superclass fix

---
+++ library/haquery/base/HaqComponentTools.hx
@@ -0,0 +1,31 @@
+package haquery.base;
+
+class HaqComponentTools 
+{
+	public static function getTemplateClass(componentClass:Class<Dynamic>) : Class<Dynamic>
+	{
+		if (componentClass == null) return null;
+		
+		var className = Type.getClassName(componentClass);
+		var n = className.lastIndexOf(".");
+		if (n > 0)
+		{
+			#if (php || neko)
+			var templateClassName = className.substr(0, n) + ".TemplateServer";
+			#elseif js
+			var templateClassName = className.substr(0, n) + ".TemplateClient";
+			#end
+			var templateClass = Type.resolveClass(templateClassName);
+			if (templateClass != null)
+			{
+				return templateClass;
+			}
+			else
+			{
+				return getTemplateClass(Type.getSuperClass(componentClass));
+			}
+		}
+		
+		return null;
+	}
+}
