6e9feefba0b77d03220376e23ea5b1f6c5ab3c23
Yaroslav Sivakov <yar3333@gmail.com>
2012-03-02 22:37:34 +0400

    * check if haXe std php lib not patched to support nativeExceptionStack

--- library/php/Stack.hx
+++ library/php/Stack.hx
@@ -4,17 +4,24 @@ class Stack
 {
 	public static function nativeExceptionStack() : Array<Hash<Dynamic>>
 	{
-		var stack = php.Lib.toHaxeArray(untyped __php__("$GLOBALS['%nativeExceptionCallStack']"));
-		for (i in 0...stack.length)
+		if (untyped __php__("isset($GLOBALS['%nativeExceptionCallStack'])"))
 		{
-			stack[i] = php.Lib.hashOfAssociativeArray(stack[i]);
-			if (stack[i].exists('args'))
+			var stack = php.Lib.toHaxeArray(untyped __php__("$GLOBALS['%nativeExceptionCallStack']"));
+			for (i in 0...stack.length)
 			{
-				var args = php.Lib.toHaxeArray(stack[i].get('args'));
-				if (args.length > 3) args[3] = php.Lib.toHaxeArray(args[3]);
-				stack[i].set('args', args);
+				stack[i] = php.Lib.hashOfAssociativeArray(stack[i]);
+				if (stack[i].exists('args'))
+				{
+					var args = php.Lib.toHaxeArray(stack[i].get('args'));
+					if (args.length > 3) args[3] = php.Lib.toHaxeArray(args[3]);
+					stack[i].set('args', args);
+				}
 			}
+			return untyped stack;
 		}
-		return untyped stack;
+		else
+		{
+			return [];
+		}
 	}
 }
