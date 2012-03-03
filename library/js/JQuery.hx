a8f4dcb27ae38fcf10e8844b38c7e81ec359d7c4
Yaroslav <Yaroslav@YHOST.beeline>
2012-03-03 21:24:42 +0400

    + JQuery.post()

--- library/js/JQuery.hx
+++ library/js/JQuery.hx
@@ -311,6 +311,8 @@ typedef JqEvent = {
 	//static function get, post
 	//static function getJSON, getScript, globalEval, grep
 	//static function is*, makeArray, map, merge, noop, now, param, proxy, sub, trim, type, unique
+	static function post(url : String, ?params : Dynamic, ?callb : String -> Void);
+	
 
 	private static inline function getCurrent() : JQuery {
 		return untyped __js__("$(this)");
