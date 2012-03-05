146c875311946adffd1f7158b39efab87ba585fd
Yaroslav <Yaroslav@YHOST.beeline>
2012-03-05 19:55:59 +0400

    * tests

--- tests/models/HaqQueryTest.hx
+++ tests/models/HaqQueryTest.hx
@@ -3,7 +3,7 @@ package models;
 import php.Lib;
 import php.NativeArray;
 import php.NativeString;
-import haxe.htmlparser.HtmlParser;
+import haxe.htmlparser.HtmlDocument;
 import haquery.server.HaqQuery;
 import haquery.server.HaqCssGlobalizer;
 
@@ -11,14 +11,14 @@ class HaqQueryTest extends haxe.unit.TestCase
 {
 	public function testGetAttr()
     {
-		var doc : HtmlDocument = new HtmlDocument('<a href="url">привет</a>');
+		var doc = new HtmlDocument('<a href="url">привет</a>');
 		var query = new HaqQuery(new HaqCssGlobalizer(''), '', 'a', doc.find('a'));
 		assertEquals('url', query.attr('href'));
     }
 	
 	public function testSetAttr()
     {
-		var doc : HtmlDocument = new HtmlDocument('<a href="url">привет</a>');
+		var doc = new HtmlDocument('<a href="url">привет</a>');
 		var query = new HaqQuery(new HaqCssGlobalizer(''), '', 'a', doc.find('a'));
 		query.attr('href', 'newurl');
 		assertEquals('newurl', query.attr('href'));
@@ -26,14 +26,14 @@ class HaqQueryTest extends haxe.unit.TestCase
 	
 	public function testGetHtml()
     {
-		var doc : HtmlDocument = new HtmlDocument('<a href="url">привет</a>');
+		var doc = new HtmlDocument('<a href="url">привет</a>');
 		var query = new HaqQuery(new HaqCssGlobalizer(''), '', 'a', doc.find('a'));
 		assertEquals('привет', query.html());
     }
 	
 	public function testSetHtml()
     {
-		var doc : HtmlDocument = new HtmlDocument('<a href="url">привет</a>');
+		var doc = new HtmlDocument('<a href="url">привет</a>');
 		var query = new HaqQuery(new HaqCssGlobalizer(''), '', 'a', doc.find('a'));
 		query.html('до свидания');
 		assertEquals('до свидания', query.html());
@@ -41,14 +41,14 @@ class HaqQueryTest extends haxe.unit.TestCase
 	
 	public function testGetVal()
     {
-		var doc : HtmlDocument = new HtmlDocument('<input type="text" value="abc" />');
+		var doc = new HtmlDocument('<input type="text" value="abc" />');
 		var query = new HaqQuery(new HaqCssGlobalizer(''), '', 'input', doc.find('input'));
 		assertEquals('abc', query.val());
     }
 	
 	public function testSetVal()
     {
-		var doc : HtmlDocument = new HtmlDocument('<input type="text" value="abc" />');
+		var doc = new HtmlDocument('<input type="text" value="abc" />');
 		var query = new HaqQuery(new HaqCssGlobalizer(''), '', 'input', doc.find('input'));
 		query.val('def');
 		assertEquals('def', query.val());
