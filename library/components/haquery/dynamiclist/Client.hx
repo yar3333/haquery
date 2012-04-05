a5d3620c7d19681f933730a27fdcf915f2210798
Yaroslav <Yaroslav@YHOST>
2012-04-06 01:55:39 +0400

    + dynamiclist v0.1

---
+++ library/components/haquery/dynamiclist/Client.hx
@@ -0,0 +1,44 @@
+package components.haquery.dynamiclist;
+
+class Client extends components.haquery.list.Client
+{
+	var docs : Hash<String>;
+	
+	function init()
+    {
+        docs = Unserializer.run(q("#docs").val()); q("#docs").remove();
+    }
+	
+	public function create(parentElem:JQuery, factoryInitParams:Array<Dynamic>)
+	{
+		if (factoryInitParams == null) factoryInitParams = [];
+		
+		var doc = new JQuery(docStr);
+		for (node in doc)
+		{
+			prepareDoc(node, "c" + Std.string(length));
+		}
+		doc.appendTo(parentElem);
+		
+		HaqElemEventManager.elemsWasChanged();
+		
+		manager.createComponent(this, "dynamiclistitem", "c" + Std.string(length), factoryInitParams);
+		
+		q('#length').val(length + 1);
+	}
+	
+	function prepareDoc(node:JQuery, childID:String)
+	{
+		var id = node.attr("id");
+		
+		if (id != "")
+		{
+			node.attr("id", prefixID + childID + HaqDefines.DELIMITER + id);
+		}
+		
+		for (child in node.children())
+		{
+			prepareDoc(child, childID);
+		}
+	}
+}
