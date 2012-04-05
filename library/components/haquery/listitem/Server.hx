a0958229c4d88bbcdbac6525772936f6bbb5253a
Yaroslav Sivakov <yar3333@gmail.com>
2012-04-06 01:53:20 +0400

    * listitem minor unification

--- library/components/haquery/listitem/Server.hx
+++ library/components/haquery/listitem/Server.hx
@@ -2,7 +2,6 @@ package components.haquery.listitem;
 
 import haquery.server.HaqComponent;
 import haquery.server.HaqTemplateManager;
-import haquery.server.HaqTemplate;
 import haxe.htmlparser.HtmlDocument;
 import haxe.htmlparser.HtmlNodeElement;
 
@@ -10,34 +9,7 @@ class Server extends Base
 {
 	override public function construct(manager:HaqTemplateManager, fullTag:String, parent:HaqComponent, id:String, doc:HtmlDocument, params:Hash<String>, innerNode:HtmlNodeElement, isInnerComponent:Bool):Void 
 	{
-		var innerHTML = innerNode.innerHTML;
-        
-        if (params != null)
-		{
-            var reConsts = new EReg("[{]([_a-zA-Z][_a-zA-Z0-9]*)[}]", "");
-            
-            innerHTML = reConsts.customReplace(innerHTML, function(re) 
-            {
-                var const = re.matched(1);
-                if (params.exists(const))
-                {
-                    return params.get(const);
-                }
-                return re.matched(0);
-            });
-        }
-        
-        var xml = null;
-        try
-        {
-            xml = new HtmlDocument(innerHTML);
-        }
-        catch (e:Dynamic)
-        {
-            trace("XML parse error:\n" + innerHTML);
-            xml = new HtmlDocument('XML parse error.');
-        }
-        
+        var xml = Tools.applyHtmlParams(innerNode.innerHTML, params);
         super.construct(manager, fullTag, parent, id, xml, params, null, isInnerComponent);
 	}
     
---
+++ library/components/haquery/listitem/Tools.hx
@@ -0,0 +1,37 @@
+package components.haquery.listitem;
+
+import haxe.htmlparser.HtmlDocument;
+
+class Tools
+{
+    static public function applyHtmlParams(html:String, params:Hash<String>) : HtmlDocument
+    {
+        if (params != null)
+		{
+            var reConsts = new EReg("[{]([_a-zA-Z][_a-zA-Z0-9]*)[}]", "");
+            
+            html = reConsts.customReplace(html, function(re) 
+            {
+                var const = re.matched(1);
+                if (params.exists(const))
+                {
+                    return params.get(const);
+                }
+                return re.matched(0);
+            });
+        }
+        
+        var xml = null;
+        try
+        {
+            xml = new HtmlDocument(html);
+        }
+        catch (e:Dynamic)
+        {
+            trace("XML parse error:\n" + html);
+            xml = new HtmlDocument("XML parse error.");
+        }
+        
+        return xml;
+    }
+}
