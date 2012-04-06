702ce671eaac9d84267a3bd6a10cc0d7c1622fc0
Yaroslav Sivakov <yar3333@gmail.com>
2012-04-06 19:04:17 +0400

    + dynamiclist v1.0

---
+++ tools/examples/dynamiclists/src/components/dynamiclists/item/Client.hx
@@ -0,0 +1,14 @@
+package components.dynamiclists.item;
+
+import haquery.client.Lib;
+import haquery.client.HaqComponent;
+
+class Client extends HaqComponent
+{
+    //var template : TemplateClient;
+
+    function test_click()
+    {
+        trace("test_click on client");
+    }
+}
---
+++ tools/examples/dynamiclists/src/components/dynamiclists/item/Server.hx
@@ -0,0 +1,14 @@
+package components.dynamiclists.item;
+
+import haquery.server.Lib;
+import haquery.server.HaqComponent;
+
+class Server extends HaqComponent
+{
+    //var template : TemplateServer;
+    
+    function test_click()
+    {
+        trace("test_click on server");
+    }
+}
