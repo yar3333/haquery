358c1ceccc66c3a0139471e16d7f083b7c7343bb
Yaroslav Sivakov <yar3333@gmail.com>
2012-04-06 15:14:28 +0400

    + dynamiclist v0.2

--- library/components/haquery/dynamiclist/Client.hx
+++ library/components/haquery/dynamiclist/Client.hx
@@ -1,5 +1,8 @@
 package components.haquery.dynamiclist;
 
+import haxe.Unserializer; 
+import js.JQuery;
+
 class Client extends components.haquery.list.Client
 {
 	var docs : Hash<String>;
@@ -9,57 +12,9 @@ class Client extends components.haquery.list.Client
         docs = Unserializer.run(q("#docs").val()); q("#docs").remove();
     }
 	
-	public function create(parentElem:JQuery, factoryInitParams:Array<Dynamic>)
+	public function create(parentElem:JQuery, params:Dynamic)
 	{
-		if (factoryInitParams == null) factoryInitParams = [];
-		
-		var doc = new JQuery(docStr);
-		for (node in doc)
-		{
-			prepareDoc(node, "c" + Std.string(length));
-		}
-		doc.appendTo(parentElem);
-		
-		HaqElemEventManager.elemsWasChanged();
-		
-		manager.createComponent(this, "dynamiclistitem", "c" + Std.string(length), factoryInitParams);
-		
+		manager.createComponent(this, "dynamiclistitem", Std.string(length), [ parentElem, docs, params ]);
 		q('#length').val(length + 1);
 	}
-	
-	function prepareDoc(node:JQuery, childID:String)
-	{
-		var id = node.attr("id");
-		
-		if (id != "")
-		{
-			node.attr("id", prefixID + childID + HaqDefines.DELIMITER + id);
-		}
-		
-		for (child in node.children())
-		{
-			prepareDoc(child, childID);
-		}
-	}
-	
-	
-	public function createDocComponents(parent:HaqComponent, baseNode:HtmlNodeElement, isCustomRender:Bool) : Array<HaqComponent>
-    {
-		var r = [];
-		
-		for (node in baseNode.children)
-        {
-            if (node.name.startsWith('haq:'))
-            {
-				var tag = node.name.substr('haq:'.length).replace("-", ".");
-				r.push(createComponent(parent, tag, node.getAttribute('id'), node.getAttributesAssoc(), node, isCustomRender));
-            }
-			else
-			{
-				r = r.concat(createDocComponents(parent, node, isCustomRender));
-			}
-        }
-		
-		return r;
-    }	
 }
--- library/components/haquery/dynamiclistitem/Client.hx
+++ library/components/haquery/dynamiclistitem/Client.hx
@@ -1,44 +1,100 @@
 package components.haquery.dynamiclistitem;
 
+import js.JQuery;
+import haquery.HashTools;
+import haquery.client.HaqComponent;
+import haxe.htmlparser.HtmlDocument;
+import haxe.htmlparser.HtmlNodeElement;
+
+using haquery.StringTools;
+
+private typedef ComponentData =
+{
+	var fullTag : String;
+	var prefixID : String;
+	var id : String;
+	var params : Hash<String>;
+	var chilren : Array<ComponentData>;
+}
+
 class Client extends components.haquery.listitem.Client
 {
-	var docs : Hash<String>;
+	var nextChildID : Int;
 	
-	function init()
-    {
-        docs = Unserializer.run(q("#docs").val()); q("#docs").remove();
-    }
-	
-	public function create(parentElem:JQuery, factoryInitParams:Array<Dynamic>)
+	function new()
 	{
-		if (factoryInitParams == null) factoryInitParams = [];
-		
-		var doc = new JQuery(docStr);
-		for (node in doc)
-		{
-			prepareDoc(node, "c" + Std.string(length));
-		}
-		doc.appendTo(parentElem);
-		
-		HaqElemEventManager.elemsWasChanged();
-		
-		manager.createComponent(this, "dynamiclistitem", "c" + Std.string(length), factoryInitParams);
-		
-		q('#length').val(length + 1);
+		super();
+		nextChildID = 1;
 	}
 	
-	function prepareDoc(node:JQuery, childID:String)
+	function factoryInit(parentElem:JQuery, docs:Hash<String>, params:Dynamic)
 	{
-		var id = node.attr("id");
+		var html = docs.get("");
+		html = components.haquery.listitem.Tools.apply(html, HashTools.hashify(params));
 		
-		if (id != "")
+		var doc = new HtmlDocument(html);
+		prepareDoc(parent.parent.fullTag, prefixID, doc);
+		
+		var components = parentElem.append(doc.innerHTML);
+		HaqElemEventManager.elemsWasChanged();
+		
+		
+	}
+	
+	function prepareDoc(fullTag:String, prefixID:String, node:HtmlNodeElement) : Array<ComponentData>
+	{
+		var r = new Array<ComponentData>();
+		
+		if (!node.name.startWith("haq:") && node.hasAttribute("id"))
 		{
-			node.attr("id", prefixID + childID + HaqDefines.DELIMITER + id);
+			var id = node.getAttribute("id");
+			if (id != "")
+			{
+				node.attr("id", prefixID + id);
+			}
 		}
-		
+			
 		for (child in node.children())
 		{
-			prepareDoc(child, childID);
+			if (!child.name.startWith("haq:"))
+			{
+				prepareDoc(fullTag, prefixID, child);
+			}
+			else
+			{
+				var id = getComponentID(child);
+				var t = manager.findTemplate(fullTag, child.name.substr("haq:".length));
+				var html = docs.get(t.fullTag);
+				var doc = new HtmlDocument(html);
+				r.push({ fullTag:t.fullTag, prefixID:prefixID, id:id, params:child.getAttributes(), chilren:prepareDoc(t.fullTag, prefixID + id, doc) });
+				child.replace(doc);
+			}
 		}
+		
+		return r;
+	}
+	
+	function createChildComponents(parent:HaqComponent, components:Array<ComponentData>)
+	{
+		for (c in components)
+		{
+			var pc = manager.createComponent(parent, c.fullTag, c.id, c.params);
+			createChildComponents(pc, c.chilren);
+		}
+	}
+
+	function getComponentID(node:HtmlNodeElement) : String
+	{
+		var id = "";
+		if (node.hasAttribute("id"))
+		{
+			id = node.getAttribute("id");
+		}
+		if (id == "")
+		{
+			id = "c" + nextChildID;
+			nextChildID++;
+		}
+		return id;
 	}
 }
