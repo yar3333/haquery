f53c142e9db1a0d465a67101ea50d5211740742c
Yaroslav <Yaroslav@YHOST>
2012-04-10 18:50:31 +0400

    * init() now always without arguments

--- library/components/haquery/dynamicfactory/Client.hx
+++ library/components/haquery/dynamicfactory/Client.hx
@@ -1,5 +1,7 @@
 package components.haquery.dynamicfactory;
 
+import haquery.client.HaqComponent;
+import haquery.client.HaqEvent;
 import haxe.htmlparser.HtmlNodeElement;
 import haxe.Unserializer; 
 import js.JQuery;
@@ -13,10 +15,11 @@ class Client extends components.haquery.factory.Client
         html = Unserializer.run(q("#html").val()); q("#html").remove();
     }
 	
-	public function create(parentElem:JQuery, params:Dynamic)
+	public function create(parentElem:JQuery, params:Dynamic) : HaqComponent
 	{
 		var n = length;
-		manager.createComponent(this, "components.haquery.dynamicfactoryitem", Std.string(n), true, { parentElem:parentElem, html:html, params:params });
+		var r = manager.createComponent(this, "components.haquery.dynamicfactoryitem", Std.string(n), true, { parentElem:parentElem, html:html, params:params } );
 		q('#length').val(n + 1);
+		return r;
 	}
 }
