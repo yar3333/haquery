b9c6e3a2f14c2b0d55b247b8469baa372123b73a
Yaroslav Sivakov <yar3333@gmail.com>
2012-04-16 20:22:30 +0400

    + list's itemDataBound event

--- library/components/haquery/list/Server.hx
+++ library/components/haquery/list/Server.hx
@@ -1,16 +1,20 @@
 package components.haquery.list;
 
 import haquery.server.Lib;
+import haquery.server.HaqEvent;
 
 class Server extends components.haquery.factory.Server
 {
+	public var event_itemDataBound : HaqEvent;
+	
 	public function bind(objects:Iterable<Dynamic>)
     {
         Lib.assert(!Lib.isPostback, "List binding on postback is not allowed.");
 		
         for (obj in objects)
         {
-            create(obj);
+            var item = create(obj);
+			event_itemDataBound.call([ item, obj ]);
         }
     }
 }
