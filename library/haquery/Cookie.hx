c01305b756a4bae101e42eec5a6f00485228583f
Yaroslav <Yaroslav@YHOST>
2012-05-06 02:29:13 +0400

    * neko support fixes

--- library/haquery/Cookie.hx
+++ library/haquery/Cookie.hx
@@ -1,44 +1,52 @@
 package haquery;
 
+#if !client
+	#if php
+	private typedef Web = php.Web;
+	#elseif neko
+	private typedef Web = neko.Web;
+	#end
+#end
+
 class Cookie 
 {
     public static function all() : Hash<String>
     {
         #if !client
-		return php.Web.getCookies();
+		return Web.getCookies();
         #else
 		return js.Cookie.all();
         #end
     }
     
-    public static function exists(name : String) : Bool
+    public static function exists(name:String) : Bool
     {
         #if !client
-		return php.Web.getCookies().exists(name);
+		return Web.getCookies().exists(name);
         #else
 		return js.Cookie.exists(name);
         #end
     }
     
-    public static function get(name : String)
+    public static function get(name:String) : String
     {
         #if !client
-		return php.Web.getCookies().get(name);
+		return Web.getCookies().get(name);
         #else
 		return js.Cookie.get(name);
         #end
     }
 	
-	public static function set(name : String, value : String, ?expire : Date, ?path : String, ?domain : String)
+	public static function set(name:String, value:String, ?expire:Date, ?path:String, ?domain:String)
 	{
 		#if !client
-		return php.Web.setCookie(name, value, expire, domain, path);
+		return Web.setCookie(name, value, expire, domain, path);
 		#else
 		return js.Cookie.set(name, value, expire != null ? Std.int(expire.getTime() - Date.now().getTime()) : null, path, domain) ;
 		#end
 	}
     
-    public static function remove(name : String, ?path : String, ?domain : String) : Void
+    public static function remove(name:String, ?path:String, ?domain:String) : Void
     {
         #if !client
 		if (exists(name))
