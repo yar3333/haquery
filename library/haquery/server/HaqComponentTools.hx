11d96d74445ca325c61357de51cdca7b52889c62
Yaroslav Sivakov <yar3333@gmail.com>
2012-05-06 23:30:05 +0400

    * attributes => component fields neko fix

--- library/haquery/server/HaqComponentTools.hx
+++ library/haquery/server/HaqComponentTools.hx
@@ -6,22 +6,18 @@ using haquery.StringTools;
 
 class HaqComponentTools 
 {
-    static var baseComponentFields : List<String> = null;
+    static var baseComponentFields : Array<String> = null;
 	
 	public static function getFieldsToLoadParams(component:HaqComponent) : Hash<String>
     {
 		if (baseComponentFields == null)
 		{
-			var emptyComponent = Type.createEmptyInstance(HaqComponent);
-			baseComponentFields = Lambda.filter(
-				 Reflect.fields(emptyComponent)
-				,function(field) return !Reflect.isFunction(Reflect.field(emptyComponent, field))
-			);
+			baseComponentFields = Type.getInstanceFields(HaqComponent);
 			baseComponentFields.push('template');
 		}
 		
 		var r : Hash<String> = new Hash<String>(); // fieldname => FieldName
-        for (field in Reflect.fields(component))
+        for (field in Type.getInstanceFields(Type.getClass(component)))
         {
             if (!Reflect.isFunction(Reflect.field(component, field))
 			 && (field == 'visible' || !Lambda.has(baseComponentFields, field))
@@ -30,7 +26,8 @@ class HaqComponentTools
                 r.set(field.toLowerCase(), field);
             }
         }
-        return r;
+		
+		return r;
     }
 	
 	public static function expandDocElemIDs(prefixID:String, baseNode:HtmlNodeElement) : Void
