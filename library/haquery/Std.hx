6e86727d9917d5c81c99cb7b9803d75b75bded71
Yaroslav Sivakov <yar3333@gmail.com>
2012-05-06 20:31:34 +0400

    - root typedef for std classes overloading

--- library/haquery/Std.hx
+++ library/haquery/Std.hx
@@ -4,12 +4,12 @@ import Type.ValueType;
 
 class Std 
 {
-	public static inline function is( v : Dynamic, t : Dynamic ) : Bool { return HaxeStd.is(v, t);  }
-	public static inline function string( s : Dynamic ) : String { return HaxeStd.string(s);  }
-	public static inline function int( x : Float ) : Int { return HaxeStd.int(x);  }
-	public static inline function parseInt( x : String ) : Null<Int> { return HaxeStd.parseInt(x);  }
-	public static inline function parseFloat( x : String ) : Float { return HaxeStd.parseFloat(x);  }
-	public static inline function random( x : Int ) : Int { return HaxeStd.random(x);  }
+	public static inline function is( v : Dynamic, t : Dynamic ) : Bool { return std.Std.is(v, t);  }
+	public static inline function string( s : Dynamic ) : String { return std.Std.string(s);  }
+	public static inline function int( x : Float ) : Int { return std.Std.int(x);  }
+	public static inline function parseInt( x : String ) : Null<Int> { return std.Std.parseInt(x);  }
+	public static inline function parseFloat( x : String ) : Float { return std.Std.parseFloat(x);  }
+	public static inline function random( x : Int ) : Int { return std.Std.random(x);  }
 	
     public static function bool(v:Dynamic) : Bool
     {
