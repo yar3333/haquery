dcc117af71d08c98b3883195dcbf60924eabe28d
Yaroslav <Yaroslav@YHOST>
2012-05-07 11:31:03 +0400

    * support platform-specific files by "--<platform>" file postfix

--- tools/copyfolder/Program.cs
+++ tools/copyfolder/Program.cs
@@ -10,45 +10,72 @@ namespace copyfolder
     {
         static int Main(string[] args)
         {
-            if (args.Length == 2 || args.Length == 3)
+            if (args.Length >= 2 || args.Length <= 4)
             {
-                Regex exclude = args.Length > 2 ? new Regex(args[2], RegexOptions.IgnoreCase) : null;
-                copyFolderContent(args[0].TrimEnd("/\\".ToCharArray()), args[1].TrimEnd("/\\".ToCharArray()), exclude);
+                var platform = args.Length > 2 ? args[2] : null;
+                Regex exclude = args.Length > 3 ? new Regex(args[3], RegexOptions.IgnoreCase) : null;
+                copyFolderContent(args[0].TrimEnd("/\\".ToCharArray()), args[1].TrimEnd("/\\".ToCharArray()), platform, exclude);
                 return 0;
             }
             else
             {
                 Console.WriteLine("copyfolder utility to copy folder content.");
-                Console.WriteLine("Usage: copyfolder <from_dir> <to_dir> [exclude_regular_exprerssion]");
+                Console.WriteLine("Usage: copyfolder <from_dir> <to_dir> [<platform> [exclude_regular_exprerssion]]");
                 return 1;
             }
         }
 
-        static void copyFolderContent(string fromFolder, string toFolder, Regex exclude)
+        static void copyFolderContent(string fromFolder, string toFolder, string platform, Regex exclude)
         {
             foreach (var dir in Directory.GetDirectories(fromFolder))
             {
-                if (exclude == null || !exclude.IsMatch(dir.Replace("\\", "/")))
+                if (isAllowedForPlatform(dir, platform))
                 {
-                    copyFolderContent(dir, toFolder + '\\' + Path.GetFileName(dir), exclude);
+                    if (exclude == null || !exclude.IsMatch(dir.Replace("\\", "/")))
+                    {
+                        copyFolderContent(dir, toFolder + '\\' + getDestFileName(dir, platform), platform, exclude);
+                    }
                 }
             }
 
             foreach (var file in Directory.GetFiles(fromFolder))
             {
-                if (exclude == null || !exclude.IsMatch(file.Replace("\\", "/")))
+                if (isAllowedForPlatform(file, platform))
                 {
-                    var destFile = toFolder + '\\' + Path.GetFileName(file);
-                    if (!File.Exists(destFile) || File.GetLastWriteTime(file) > File.GetLastWriteTime(destFile))
+                    if (exclude == null || !exclude.IsMatch(file.Replace("\\", "/")))
                     {
-                        if (!Directory.Exists(toFolder))
+                        var destFile = toFolder + '\\' + getDestFileName(file, platform);
+                        if (!File.Exists(destFile) || File.GetLastWriteTime(file) > File.GetLastWriteTime(destFile))
                         {
-                            Directory.CreateDirectory(toFolder);
+                            if (!Directory.Exists(toFolder))
+                            {
+                                Directory.CreateDirectory(toFolder);
+                            }
+                            File.Copy(file, destFile, true);
                         }
-                        File.Copy(file, destFile, true);
                     }
                 }
             }
+        }
+
+        static bool isAllowedForPlatform(string path, string platform)
+        {
+            var name = Path.GetFileNameWithoutExtension(path);
+            return platform == null || !name.Contains("--") || name.EndsWith("--" + platform);
+        }
+
+        static string getDestFileName(string path, string platform)
+        {
+            var name = Path.GetFileNameWithoutExtension(path);
+            if (platform == null || !name.Contains("--"))
+            {
+                return name + Path.GetExtension(path);
+            }
+            if (name.EndsWith("--" + platform))
+            {
+                return name.Substring(0, name.Length - ("--" + platform).Length) + Path.GetExtension(path);
+            }
+            return null;
         }
     }
 }
