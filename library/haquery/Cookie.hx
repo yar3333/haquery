81215a3794ed23185bc404d3403e1518823ff5af
Yaroslav Sivakov <yar3333@gmail.com>
2012-06-20 15:51:50 +0400

    * Cookie major changes (cookie setting and right away getting now work)

--- library/haquery/Cookie.hx
+++ library/haquery/Cookie.hx
@@ -10,45 +10,56 @@ package haquery;
 
 class Cookie 
 {
-    public static function all() : Hash<String>
+    static var cookies : Hash<String>;
+	
+	static inline function init() : Void
+	{
+		if (cookies == null)
+		{
+			#if !client
+			cookies = Web.getCookies();
+			#else
+			cookies = js.Cookie.all();
+			#end
+		}
+	}
+	
+	public static function all() : Hash<String>
     {
-        #if !client
-		return Web.getCookies();
-        #else
-		return js.Cookie.all();
-        #end
+		init();
+		return cookies;
     }
     
     public static function exists(name:String) : Bool
     {
-        #if !client
-		return Web.getCookies().exists(name);
-        #else
-		return js.Cookie.exists(name);
-        #end
+        init();
+		return cookies.exists(name);
     }
     
     public static function get(name:String) : String
     {
-        #if !client
-		return Web.getCookies().get(name);
-        #else
-		return js.Cookie.get(name);
-        #end
+        init();
+		return cookies.get(name);
     }
 	
-	public static function set(name:String, value:String, ?expire:Date, ?path:String, ?domain:String)
+	public static function set(name:String, value:String, ?expire:Date, ?path:String, ?domain:String) : Void
 	{
+		init();
+		cookies.set(name, value);
+		
 		#if !client
-		return Web.setCookie(name, value, expire, domain, path);
+		Web.setCookie(name, value, expire, domain, path);
 		#else
-		return js.Cookie.set(name, value, expire != null ? Std.int(expire.getTime() - Date.now().getTime()) : null, path, domain) ;
+		js.Cookie.set(name, value, expire != null ? Std.int(expire.getTime() - Date.now().getTime()) : null, path, domain) ;
 		#end
 	}
     
     public static function remove(name:String, ?path:String, ?domain:String) : Void
     {
-        #if !client
+		init();
+        cookies.remove(name);
+		
+		#if !client
 		if (exists(name))
 		{
 			set(name, null, new Date(2000,1,1,0,0,0), domain, path);
