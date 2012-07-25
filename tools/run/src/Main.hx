8b67ccc7357ff70e247161bc9cfa43b35e00579a
Yaroslav Sivakov <yar3333@gmail.com>
2012-07-25 16:06:19 +0400

    * gen-orm: say error if databaseConnectionString not found

--- tools/run/src/Main.hx
+++ tools/run/src/Main.hx
@@ -30,8 +30,21 @@ class Main
         {
             case 'gen-orm': 
 				var project = new FlashDevelopProject("", exeDir);
-				var config = new HaqConfig(project.srcPath + "config.xml");
-				tasks.genOrm(args.length > 1 ? args[1] : config.databaseConnectionString, project.srcPath);
+				var databaseConnectionString = args.length > 1 ? args[1] : new HaqConfig(project.srcPath + "config.xml").databaseConnectionString;
+				if (databaseConnectionString != null && databaseConnectionString != "")
+				{
+					tasks.genOrm(databaseConnectionString, project.srcPath);
+				}
+				else
+				{
+					Lib.println("ERROR: databaseConnectionString not found.");
+					Lib.println("You may specify it in the 'src/config.xml' file:");
+					Lib.println("\t<config>");
+					Lib.println("\t\t<param name=\"databaseConnectionString\" value=\"mysql://USER:PASSWORD@HOST/DATABASE\" />");
+					Lib.println("\t</config>");
+					Lib.println("or in the command line:");
+					Lib.println("\thaxelib run HaQuery gen-orm mysql://USER:PASSWORD@HOST/DATABASE");
+				}
 			
 			case 'pre-build': 
                 tasks.preBuild();
