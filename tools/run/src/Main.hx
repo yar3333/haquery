446912252b612feef79c89239f2c6241c2ad7b4b
Yaroslav <Yaroslav@YHOST>
2012-08-07 16:03:41 +0400

    + CommandLineOptionsParser

--- tools/run/src/Main.hx
+++ tools/run/src/Main.hx
@@ -1,6 +1,7 @@
 package ;
 
 import haquery.server.HaqConfig;
+import haquery.tools.CommandLineOptionsParser;
 import haquery.tools.FlashDevelopProject;
 import neko.Lib;
 import neko.Sys;
@@ -26,45 +27,56 @@ class Main
 		
 		var tasks = new Tasks(exeDir);
         
-        switch (args.length > 0 ? args[0] : '')
-        {
-            case 'gen-orm': 
-				var project = new FlashDevelopProject("", exeDir);
-				var databaseConnectionString = args.length > 1 ? args[1] : new HaqConfig(project.srcPath + "config.xml").databaseConnectionString;
-				if (databaseConnectionString != null && databaseConnectionString != "")
-				{
-					tasks.genOrm(databaseConnectionString, project);
-				}
-				else
-				{
-					Lib.println("ERROR: databaseConnectionString not found.");
-					Lib.println("You may specify it in the 'src/config.xml' file:");
-					Lib.println("\t<config>");
-					Lib.println("\t\t<param name=\"databaseConnectionString\" value=\"mysql://USER:PASSWORD@HOST/DATABASE\" />");
-					Lib.println("\t</config>");
-					Lib.println("or in the command line:");
-					Lib.println("\thaxelib run HaQuery gen-orm mysql://USER:PASSWORD@HOST/DATABASE");
-				}
-			
-			case 'pre-build': 
-                tasks.preBuild();
-            
-            case 'post-build': 
-				return tasks.postBuild() ? 0 : 1;
-                
-            case 'install':
-                tasks.install();
-            
-            default:
-				Lib.println("HaQuery building support and deploying tool.");
-				Lib.println("Usage: haxelib run HaQuery <command>");
-				Lib.println("\twhere <command> may be:");
-				Lib.println("\t\tpre-build                           Do pre-build step.");
-				Lib.println("\t\tpost-build                          Do post-build step.");
-				Lib.println("\t\tinstall                             Install FlashDevelop templates.");
-				Lib.println("\t\tgen-orm [databaseConnectionString]  Generate object-related classes (managers and models).");
-                return 1;
+        if (args.length > 0)
+		{
+			var command = args.shift();
+			switch (command)
+			{
+				case 'gen-orm': 
+					//var parser = new CommandLineOptionsParser();
+					//parser.addOption([ "-pmo", "--package-models" ]
+					
+					var project = new FlashDevelopProject("", exeDir);
+					var databaseConnectionString = args.length > 1 ? args[1] : new HaqConfig(project.srcPath + "config.xml").databaseConnectionString;
+					if (databaseConnectionString != null && databaseConnectionString != "")
+					{
+						tasks.genOrm(databaseConnectionString, project);
+					}
+					else
+					{
+						Lib.println("ERROR: databaseConnectionString not found.");
+						Lib.println("You may specify it in the 'src/config.xml' file:");
+						Lib.println("\t<config>");
+						Lib.println("\t\t<param name=\"databaseConnectionString\" value=\"mysql://USER:PASSWORD@HOST/DATABASE\" />");
+						Lib.println("\t</config>");
+						Lib.println("or in the command line:");
+						Lib.println("\thaxelib run HaQuery gen-orm mysql://USER:PASSWORD@HOST/DATABASE");
+					}
+				
+				case 'pre-build': 
+					tasks.preBuild();
+				
+				case 'post-build': 
+					return tasks.postBuild() ? 0 : 1;
+					
+				case 'install':
+					tasks.install();
+				
+				default:
+					Lib.println("ERROR: command '" + command + "' is not supported.");
+			}
         }
+		else
+		{
+			Lib.println("HaQuery building support and deploying tool.");
+			Lib.println("Usage: haxelib run HaQuery <command>");
+			Lib.println("\twhere <command> may be:");
+			Lib.println("\t\tpre-build                           Do pre-build step.");
+			Lib.println("\t\tpost-build                          Do post-build step.");
+			Lib.println("\t\tinstall                             Install FlashDevelop templates.");
+			Lib.println("\t\tgen-orm [databaseConnectionString]  Generate object-related classes (managers and models).");
+			return 1;
+		}
         
         return 0;
 	}
