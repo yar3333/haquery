b06824043bb3c60c2788bdeeb84ca4935de17581
Yaroslav <Yaroslav@YHOST.beeline>
2012-10-24 15:30:38 +0400

    - IntTools

--- library/haquery/server/HaqWebsocketServerLoop.hx
+++ library/haquery/server/HaqWebsocketServerLoop.hx
@@ -180,7 +180,7 @@ class HaqWebsocketServerLoop
 				case HaqMessageToListener.Status:
 					var s = "connected pages: " + Lambda.count(pages) + "\n"
 						  + "waited pages: " + Lambda.count(waitedPages) + "\n"
-						  + "memory heap: " + IntTools.groupDigits(Math.round(Gc.stats().heap / 1024), " ") + " KB\n";
+						  + "memory heap: " + groupDigits(Math.round(Gc.stats().heap / 1024), " ") + " KB\n";
 					client.ws.send(s);
 				
 				case HaqMessageToListener.Stop:
@@ -200,6 +200,46 @@ class HaqWebsocketServerLoop
 		if (p != null)
 		{
 			server.closeConnection(p.ws.socket);
+		}
+	}
+	
+	/**
+	* Groups the digits in the input number by using a thousands separator.<br/>
+	* E.g. the number 1024 is converted to the string '1.024'.
+	* @param thousandsSeparator a character to use as a thousands separator. The default value is ".".
+	*/
+	static function groupDigits(x:Int, ?thousandsSeparator = '.'):String
+	{
+		var n : Float = x;
+		var c = 0;
+		while (n > 1)
+		{
+			n /= 10;
+			c++;
+		}
+		c = cast c / 3;
+		var source = Std.string(x);
+		if (c == 0)
+			return source;
+		else
+		{
+			var target = '';
+
+			var i = 0;
+			var j = source.length - 1;
+			while (j >= 0)
+			{
+				if (i == 3)
+				{
+					target = source.charAt(j--) + thousandsSeparator + target;
+					i = 0;
+					c--;
+				}
+				else
+					target = source.charAt(j--) + target;
+				i++;
+			}
+			return target;
 		}
 	}
 }
