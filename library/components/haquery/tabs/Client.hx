e72bfcb3ba5a4118715978d4e680510df7e7c315
Yaroslav <Yaroslav@YHOST.beeline>
2012-12-22 01:42:19 +0400

    * tabs minor refactoring

--- library/components/haquery/tabs/Client.hx
+++ library/components/haquery/tabs/Client.hx
@@ -1,23 +1,24 @@
 package components.haquery.tabs;
 
-import haquery.client.HaqQuery;
 import haquery.common.HaqEvent;
-import js.JQuery;
 
 class Client extends BaseClient
 {
-    public var active(getActive, setActive) : Int;
+	var event_change : HaqEvent<{ active:Int }>;
+    
+	public var active(getActive, setActive) : Int;
 
     function init()
     {
         var self = this;
         
-        var tabs = q('#tabs>*:eq(0)>*').get();
+        var tabs = template().container.find('>*:eq(0)>*').get();
 		for (i in 0...tabs.length)
 		{
 			q(tabs[i]).click(function(e)
             {
                 self.active = i;
+				event_change.call( { active:i } );
             });
 		}
         
@@ -26,7 +27,7 @@ class Client extends BaseClient
     
     function getActive() : Int
     {
-        var panels = q('#tabs>*:eq(1)>*').get();
+        var panels = template().container.find('>*:eq(1)>*').get();
 		for (i in 0...panels.length)
 		{
 			if (q(panels[i]).hasClass('active')) return i;
@@ -36,10 +37,12 @@ class Client extends BaseClient
 
     function setActive(n:Int) : Int
     {
-        q('#tabs>*:eq(0)>*').removeClass('active');
-        q('#tabs>*:eq(0)>*:eq(' + n + ')').addClass('active');
-        q('#tabs>*:eq(1)>*').removeClass('active').hide();
-        q('#tabs>*:eq(1)>*:eq(' + n + ')').addClass('active').show();
+        var container = template().container;
+		
+		container.find('>*:eq(0)>*').removeClass('active');
+        container.find('>*:eq(0)>*:eq(' + n + ')').addClass('active');
+        container.find('>*:eq(1)>*').removeClass('active').hide();
+        container.find('>*:eq(1)>*:eq(' + n + ')').addClass('active').show();
         
         return n;
     }
--- library/components/haquery/tabs/Server.hx
+++ library/components/haquery/tabs/Server.hx
@@ -1,41 +1,36 @@
 package components.haquery.tabs;
 
-import haquery.server.HaqQuery;
 import haquery.server.Lib;
-import haxe.htmlparser.HtmlParser;
 
 class Server extends BaseServer
 {
     public var active = 0;
     
-    function init()
+    function preRender()
     {
-        if (!page.isPostback)
-        {
-            var buttonsAndPanels = innerNode.children;
-            Lib.assert(buttonsAndPanels.length == 2, "Tabs component must contain exactly two subelements.");
-            
-            var buttons = buttonsAndPanels[0].children;
-            var i = 0;
-            for (child in buttons)
-            {
-                if (i == active)
-				{
-					q(child).addClass('active');
-				}
-                i++;
-            }
-            
-            var panels = buttonsAndPanels[1].children;
-            var j = 0;
-            for (child in panels)
-            {
-                if (j == active)
-				{
-					q(child).addClass('active');
-				}
-                j++;
-            }
-        }
+		var buttonsAndPanels = innerNode.children;
+		Lib.assert(buttonsAndPanels.length == 2, "Tabs component must contain exactly two subelements.");
+		
+		var buttons = buttonsAndPanels[0].children;
+		var i = 0;
+		for (child in buttons)
+		{
+			if (i == active)
+			{
+				q(child).addClass('active');
+			}
+			i++;
+		}
+		
+		var panels = buttonsAndPanels[1].children;
+		var j = 0;
+		for (child in panels)
+		{
+			if (j == active)
+			{
+				q(child).addClass('active');
+			}
+			j++;
+		}
     }
 }
