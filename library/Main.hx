9e480ad630c0939a32f85f8ce40a4c8147b3ed94
Yaroslav Sivakov <yar3333@gmail.com>
2013-01-21 15:19:50 +0400

    + class Main

---
+++ library/Main.hx
@@ -0,0 +1,16 @@
+import Imports;
+
+class Main 
+{
+	static function main() 
+	{
+		#if server
+			#if php
+			haquery.server.Lib.run();
+			#elseif neko
+			neko.Web.cacheModule(haquery.server.Lib.run);
+			haquery.server.Lib.run();
+			#end
+		#end
+ 	}
+}
