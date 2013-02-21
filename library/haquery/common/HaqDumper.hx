b4a72f36b300dbdf954bcefe427288d309886eba
Yaroslav Sivakov <yar3333@gmail.com>
2013-02-21 10:22:46 +0400

    * dumper fix

--- library/haquery/common/HaqDumper.hx
+++ library/haquery/common/HaqDumper.hx
@@ -2,8 +2,10 @@ package haquery.common;
 
 class HaqDumper 
 {
-	public static function getDump(v:Dynamic, level=0) : String
+	public static function getDump(v:Dynamic, limit=3, level=0) : String
 	{
+		if (level >= limit) return "...";
+		
 		var prefix = ""; for (i in 0...level) prefix += "\t";
 		
 		var s : String;
@@ -26,7 +28,7 @@ class HaqDumper
 					s = "ARRAY(" + v.length + ")\n";
 					for (item in cast(v, Array<Dynamic>))
 					{
-						s += getDump(item, level + 1);
+						s += getDump(item, limit, level + 1);
 					}
 				}
 				else
@@ -35,12 +37,12 @@ class HaqDumper
 					s = "HASH\n";
 					for (key in cast(v, Hash<Dynamic>).keys())
 					{
-						s += prefix + key + " => " + getDump(v.get(key), level + 1);
+						s += prefix + key + " => " + getDump(v.get(key), limit, level + 1);
 					}
 				}
 				else
 				{
-					s = "CLASS(" + Type.getClassName(c) + ")\n" + getObjectDump(v, level + 1);
+					s = "CLASS(" + Type.getClassName(c) + ")\n" + getObjectDump(v, limit, level + 1);
 				}
 			
 			case ValueType.TEnum(e):
@@ -53,7 +55,7 @@ class HaqDumper
 				s = "INT(" + Std.string(v) + ")";
 			
 			case ValueType.TObject:
-				s = "OBJECT" + "\n" + getObjectDump(v, level + 1);
+				s = "OBJECT" + "\n" + getObjectDump(v, limit, level + 1);
 			
 			case ValueType.TFunction, ValueType.TUnknown:
 				s = "FUNCTION OR UNKNOW";
@@ -61,13 +63,13 @@ class HaqDumper
 		return s != "" ? s + "\n" : "";
 	}
 	
-	static function getObjectDump(obj:Dynamic, level:Int) : String
+	static function getObjectDump(obj:Dynamic, limit, level:Int) : String
 	{
 		var prefix = ""; for (i in 0...level) prefix += "\t";
 		var s = "";
 		for (fieldName in Reflect.fields(obj))
 		{
-			s += prefix + fieldName + " : " + getDump(Reflect.field(obj, fieldName), level);
+			s += prefix + fieldName + " : " + getDump(Reflect.field(obj, fieldName), limit, level);
 		}
 		return s;
 	}	
