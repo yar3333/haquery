50501459507dbf2952d72713516b3eaddbc6556e
Yaroslav <Yaroslav@localhost>
2013-04-17 18:11:19 +0400

    * minor

--- library/components/haquery/list/Client.hx
+++ library/components/haquery/list/Client.hx
@@ -1,5 +1,11 @@
 package components.haquery.list;
 
-class Client extends Base
+class Client extends BaseClient
 {
+	public var length(get_length, null) : Int;
+    
+    function get_length() : Int
+    {
+        return Std.parseInt(q('#length').val());
+    }
 }
