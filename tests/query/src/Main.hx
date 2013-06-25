af9965a31ab940c22aeb54032ea734bd038b8d0f
Yaroslav <Yaroslav@localhost>
2013-06-25 22:53:58 +0400

    + HaqQuery test

---
+++ tests/query/src/Main.hx
@@ -0,0 +1,19 @@
+package ;
+
+#if php
+import php.Web;
+import php.Lib;
+#elseif neko
+import neko.Web;
+import neko.Lib;
+#end
+
+class Main
+{
+    static function main()
+	{
+		var r = new haxe.unit.TestRunner();
+		r.add(new QueryTest());
+		r.run();
+	}
+}
---
+++ tests/query/src/QueryTest.hx
@@ -0,0 +1,26 @@
+package;
+
+import haxe.htmlparser.HtmlDocument;
+
+class QueryTest extends haxe.unit.TestCase
+{
+    public function testSetStyle()
+    {
+		var doc = new HtmlDocument("<test style='display:none; margin-top:5px'/>");
+		var q = new HaqQuery(doc.children);
+		
+		q.css("display", "block");
+        this.assertEquals("display:block; margin-top:5px", q.attr("style"));
+		
+		q.css("margin-top", "10px");
+        this.assertEquals("display:block; margin-top:10px", q.attr("style"));
+    }
+    
+	public function testGetStyle()
+    {
+		var doc = new HtmlDocument("<test style='display:none; margin-top:5px'/>");
+		var q = new HaqQuery(doc.children);
+        this.assertEquals("none", q.css("display"));
+        this.assertEquals("5px", q.css("margin-top"));
+    }
+}
