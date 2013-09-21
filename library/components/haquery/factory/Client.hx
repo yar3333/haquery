9507d10d40ff6f91cf5cff1d68680370cf3ed787
Yaroslav <yar3333@gmail.com>
2013-09-21 17:23:03 +0400

    * minor

--- library/components/haquery/factory/Client.hx
+++ library/components/haquery/factory/Client.hx
@@ -6,7 +6,7 @@ import js.JQuery;
 
 class Client extends BaseClient
 {
-	public function create(parentElem:JQuery, params:Dynamic, append=true) : HaqComponent
+	public function create(parentElem:JQuery, ?params:Dynamic, append=true) : HaqComponent
 	{
 		var n = length;
 		var r = Lib.manager.createComponent(this, "components.haquery.factoryitem", Std.string(n), true, { parentElem:parentElem, html:page.storage.getInstanceVar(fullID, "html"), params:params, append:append } );
