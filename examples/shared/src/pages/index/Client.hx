f285229dbe111efea8296b75dfe2ba7c12133cc8
Yaroslav Sivakov <yar3333@gmail.com>
2013-10-03 16:11:03 +0400

    * misc fixes

--- examples/shared/src/pages/index/Client.hx
+++ examples/shared/src/pages/index/Client.hx
@@ -1,19 +1,17 @@
 package pages.index;
 
-import haquery.client.Lib;
-
 class Client extends BaseClient
 {
 	function bt_click(t, e)
 	{
 		server().sharedServerMethod(10, 20, function(r)
 		{
-			Lib.alert("sharedServerMethod result = " + r);
+			js.Browser.window.alert("sharedServerMethod result = " + r);
 		});
 	}
     
 	@shared function sharedClientMethod(a:Int, b:Int) : Void
 	{
-		Lib.alert("sharedClientMethod(" + a + ", " + b + ")");
+		js.Browser.window.alert("sharedClientMethod(" + a + ", " + b + ")");
 	}
 }
