f9da954ddae8a8f010808d0189f7ce35e1c0a8e0
Yaroslav Sivakov <yar3333@gmail.com>
2013-12-19 16:40:49 +0400

    * fileupload improvements

--- library/components/haquery/fileupload/Server.hx
+++ library/components/haquery/fileupload/Server.hx
@@ -2,9 +2,16 @@ package components.haquery.fileupload;
 
 class Server extends Base
 {
-    function preRender()
+    public var action : String = null;
+    public var accept : String = null;
+	
+	function preRender()
     {
-		template().form.attr("target", prefixID + "frame");
+		template().form
+			.attr("target", prefixID + "frame")
+			.attr("action", action)
+			.attr("accept", accept);
+		
         template().frame.attr("name", prefixID + "frame");
     }
 }
