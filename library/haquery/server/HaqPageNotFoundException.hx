5660d0dd6b84c567d6997c67c520fced80d6f20a
Yaroslav Sivakov <yar3333@gmail.com>
2014-01-14 12:18:47 +0400

    * HaqPageNotFoundException now keep url

--- library/haquery/server/HaqPageNotFoundException.hx
+++ library/haquery/server/HaqPageNotFoundException.hx
@@ -1,3 +1,9 @@
 package haquery.server;
 
-class HaqPageNotFoundException extends stdlib.Exception {}
+class HaqPageNotFoundException extends stdlib.Exception
+{
+	public function new(url:String)
+	{
+		super(url);
+	}
+}
