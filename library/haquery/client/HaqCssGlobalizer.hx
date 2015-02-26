7cc85c896b320933df89edb8726a4c26d1d5425b
Yaroslav Sivakov <yar3333@gmail.com>
2015-02-27 00:40:42 +0300

    + toggleClass support local css class names

--- library/haquery/client/HaqCssGlobalizer.hx
+++ library/haquery/client/HaqCssGlobalizer.hx
@@ -10,15 +10,16 @@ class HaqCssGlobalizer extends haquery.base.HaqCssGlobalizer
 		
 		untyped 
 		{
-			jq.addClass		= function(arg) return self.fixJq(jQuery.fn.addClass.call(jq, self.className(arg)));
-			jq.removeClass	= function(arg) return self.fixJq(jQuery.fn.removeClass.call(jq, self.className(arg)));
-			jq.hasClass		= function(arg) return            jQuery.fn.hasClass.call(jq, self.className(arg));
-			jq.find			= function(arg) return self.fixJq(jQuery.fn.find.call(jq, self.selector(arg)));
-			jq.filter		= function(arg) return self.fixJq(jQuery.fn.filter.call(jq, self.selector(arg)));
-			jq.has			= function(arg) return            jQuery.fn.has.call(jq, self.selector(arg));
-			jq.is			= function(arg) return            jQuery.fn.is.call(jq, self.selector(arg));
-			jq.not			= function(arg) return self.fixJq(jQuery.fn.not.call(jq, self.selector(arg)));
-			jq.parent		= function()    return self.fixJq(jQuery.fn.parent.call(jq));
+			jq.addClass		= function(arg)		return self.fixJq(jQuery.fn.addClass.call(jq, self.className(arg)));
+			jq.removeClass	= function(arg)		return self.fixJq(jQuery.fn.removeClass.call(jq, self.className(arg)));
+			jq.toggleClass	= function(arg, b)	return self.fixJq(jQuery.fn.toggleClass.call(jq, self.className(arg), b));
+			jq.hasClass		= function(arg)		return            jQuery.fn.hasClass.call(jq, self.className(arg));
+			jq.find			= function(arg)		return self.fixJq(jQuery.fn.find.call(jq, self.selector(arg)));
+			jq.filter		= function(arg)		return self.fixJq(jQuery.fn.filter.call(jq, self.selector(arg)));
+			jq.has			= function(arg)		return            jQuery.fn.has.call(jq, self.selector(arg));
+			jq.is			= function(arg)		return            jQuery.fn.is.call(jq, self.selector(arg));
+			jq.not			= function(arg)		return self.fixJq(jQuery.fn.not.call(jq, self.selector(arg)));
+			jq.parent		= function()		return self.fixJq(jQuery.fn.parent.call(jq));
 		}
 		
 		return jq;
