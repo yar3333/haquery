8ba6d3031062a5a5d0260e174266f43a3b098380
Yaroslav Sivakov <yar3333@gmail.com>
2017-04-15 00:16:13 +0300

    * haxe 3.4.0 bugfix

--- library/haquery/client/HaqCssGlobalizer.hx
+++ library/haquery/client/HaqCssGlobalizer.hx
@@ -1,24 +1,23 @@
 package haquery.client;
 
+import js.Browser.window;
+
 class HaqCssGlobalizer extends haquery.base.HaqCssGlobalizer
 {
 	public function fixJq(jq:js.JQuery) : js.JQuery
 	{
 		var self = this;
 		
-		untyped 
-		{
-			jq.addClass		= function(arg)		return self.fixJq(jQuery.fn.addClass.call(jq, self.className(arg)));
-			jq.removeClass	= function(arg)		return self.fixJq(jQuery.fn.removeClass.call(jq, self.className(arg)));
-			jq.toggleClass	= function(arg, b)	return self.fixJq(jQuery.fn.toggleClass.call(jq, self.className(arg), b));
-			jq.hasClass		= function(arg)		return            jQuery.fn.hasClass.call(jq, self.className(arg));
-			jq.find			= function(arg)		return self.fixJq(jQuery.fn.find.call(jq, self.selector(arg)));
-			jq.filter		= function(arg)		return self.fixJq(jQuery.fn.filter.call(jq, self.selector(arg)));
-			jq.has			= function(arg)		return            jQuery.fn.has.call(jq, self.selector(arg));
-			jq.is			= function(arg)		return            jQuery.fn.is.call(jq, self.selector(arg));
-			jq.not			= function(arg)		return self.fixJq(jQuery.fn.not.call(jq, self.selector(arg)));
-			jq.parent		= function()		return self.fixJq(jQuery.fn.parent.call(jq));
-		}
+		(cast jq).addClass		= function(arg)		return self.fixJq((cast window).jQuery.fn.addClass.call(jq, self.className(arg)));
+		(cast jq).removeClass	= function(arg)		return self.fixJq((cast window).jQuery.fn.removeClass.call(jq, self.className(arg)));
+		(cast jq).toggleClass	= function(arg, b)	return self.fixJq((cast window).jQuery.fn.toggleClass.call(jq, self.className(arg), b));
+		(cast jq).hasClass		= function(arg)		return            (cast window).jQuery.fn.hasClass.call(jq, self.className(arg));
+		(cast jq).find			= function(arg)		return self.fixJq((cast window).jQuery.fn.find.call(jq, self.selector(arg)));
+		(cast jq).filter		= function(arg)		return self.fixJq((cast window).jQuery.fn.filter.call(jq, self.selector(arg)));
+		(cast jq).has			= function(arg)		return            (cast window).jQuery.fn.has.call(jq, self.selector(arg));
+		(cast jq).is			= function(arg)		return            (cast window).jQuery.fn.is.call(jq, self.selector(arg));
+		(cast jq).not			= function(arg)		return self.fixJq((cast window).jQuery.fn.not.call(jq, self.selector(arg)));
+		(cast jq).parent		= function()		return self.fixJq((cast window).jQuery.fn.parent.call(jq));
 		
 		return jq;
 	}
